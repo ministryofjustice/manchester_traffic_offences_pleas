62910e0f2cf0b435bd7b2e1e127d5e33e314caf1
Sym Roe <sym.roe@talusdesign.co.uk>
2014-06-05 17:55:10 +0100

    Working urn_required decorator

--- manchester_traffic_offences/apps/defendant/decorators.py
+++ manchester_traffic_offences/apps/defendant/decorators.py
@@ -1,17 +1,18 @@
-from django.contrib.auth.decorators import user_passes_test
-
 from defendant.utils import is_valid_urn_format
+from django.contrib.auth.decorators import user_passes_test
+from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponseRedirect
 
-def urn_required(function=None):
-    """
-    Decorator for views that checks that the user is logged in, redirecting
-    to the log-in page if necessary.
-    """
-    
-    actual_decorator = user_passes_test(
-        lambda u: hasattr(u, 'urn') and is_valid_urn_format(u.urn),
-        login_url="/"
-    )
-    if function:
-        return actual_decorator(function)
-    return actual_decorator
+
+from functools import wraps
+
+def urn_required(f):
+    def wrap(request, *args, **kwargs):
+        if 'urn' in request.session:
+            if is_valid_urn_format(request.session['urn']):
+                return f(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse_lazy('urn'))
+    wrap.__doc__=f.__doc__
+    wrap.__name__=f.__name__
+    return wrap
--- manchester_traffic_offences/apps/defendant/models.py
+++ manchester_traffic_offences/apps/defendant/models.py
@@ -1,11 +1,21 @@
 import datetime
 
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.db.models import CharField
 
-# from testing.models import Case
+from .utils import is_valid_urn_format
+
+class URNField(CharField):
+    # TODO Move this to somewhere sane
+    def validate(self, value, model_instance):
+        super(URNField, self).validate(value, model_instance)
+        if not is_valid_urn_format(value):
+            raise ValidationError("URN isn't valid")
+
 
 class Defendant(models.Model):
-    urn = models.CharField(primary_key=True, max_length=100)
+    urn = URNField(primary_key=True, max_length=100)
     case = models.ForeignKey("testing.Case")
     gender = models.CharField(blank=True, max_length=1)
     dob = models.DateField(default=datetime.datetime.today)
