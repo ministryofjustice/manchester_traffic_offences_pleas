f6fb312206fdd826fd3c0d5d1fe50aa0c5b80f2d
Sym Roe <sym.roe@talusdesign.co.uk>
2014-06-18 12:53:19 +0100

    add email sending

--- manchester_traffic_offences/apps/plea/views.py
+++ manchester_traffic_offences/apps/plea/views.py
@@ -1,3 +1,6 @@
+import json
+
+from django.core.mail import send_mail
 from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy
 
@@ -24,3 +27,20 @@ class ExampleStep(BaseStepFormView):
 
 class ReviewStep(TemplateView):
     template_name = "plea/review.html"
+    
+class CompleteStep(TemplateView):
+    http_method_names = ['post',]
+    template_name = "plea/complete.html"
+    
+    def post(self, request, *args, **kwargs):
+        # Send the email
+        # TODO Extract this out properly.
+        send_mail(
+            '[TEST] A plea has been submitted',
+            json.dumps(self.request.session['plea']),
+            'sym.roe@digital.justice.gov.uk',
+            ['manchesterteam@digital.justice.gov.uk'],
+            fail_silently=False
+        )
+        del self.request.session['plea']
+        return self.get(request)
