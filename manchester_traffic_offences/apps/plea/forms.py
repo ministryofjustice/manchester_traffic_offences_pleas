ae2b410603bc5cf1a1cf2db10411e2b998baa002
Sym Roe <sym.roe@talusdesign.co.uk>
2014-06-19 12:30:37 +0100

    Use formwizzard

--- manchester_traffic_offences/apps/plea/forms.py
+++ manchester_traffic_offences/apps/plea/forms.py
@@ -1,15 +1,25 @@
 from django import forms
+from django.forms.widgets import Textarea
 
 
 class BasePleaStepForm(forms.Form):
-    step_name = None
+    pass
+    # step_name = None
 
-    def save(self, request):
-        assert self.step_name is not None, "step_name is not defined"
-        plea = request.session.get('plea', {})
-        plea[self.step_name] = self.cleaned_data
-        request.session['plea'] = plea
+    # def save(self, request):
+    #     assert self.step_name is not None, "step_name is not defined"
+    #     plea = request.session.get('plea', {})
+    #     plea[self.step_name] = self.cleaned_data
+    #     request.session['plea'] = plea
     
-class ExampleStep(BasePleaStepForm):
-    step_name = "example"
-    subject = forms.CharField(max_length=100)
+
+class AboutForm(BasePleaStepForm):
+    name = forms.CharField(max_length=255)
+    date_of_hearing = forms.SplitDateTimeField()
+    urn = forms.CharField(max_length=255)
+
+class PleaInfoForm(BasePleaStepForm):
+    mitigations = forms.CharField(widget=Textarea())
+    guilty = forms.BooleanField()
+    understand = forms.BooleanField()
+    
