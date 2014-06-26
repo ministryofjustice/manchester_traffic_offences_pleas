803ffc54f7d7c327eaa7d2aebb067f9c6648e392
Ian George <ian.george@digital.justice.gov.uk>
2014-06-26 16:32:36 +0100

    Clearing session on the app home page

--- manchester_traffic_offences/urls.py
+++ manchester_traffic_offences/urls.py
@@ -8,13 +8,13 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 from apps.defendant.views import DefendantLogin
-
+from .views import HomeView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^$', TemplateView.as_view(template_name="start.html")),
+                       url(r'^$', HomeView.as_view()),
                        url(r'^urn/', DefendantLogin.as_view(), name="urn"),
                        url(r'^plea/', include('plea.urls', )),
                        url(r'^feedback/', TemplateView.as_view(
