c1ad5a6ff2e7a1b4676b914fb066f7329f57de01
Ian George <ian.george@digital.justice.gov.uk>
2014-06-26 14:37:51 +0100

    Dealing with email errors (smtplib, gaierror, socket.error), added failure / retry view

--- manchester_traffic_offences/apps/plea/views.py
+++ manchester_traffic_offences/apps/plea/views.py
@@ -1,3 +1,6 @@
+import smtplib
+import socket
+
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
@@ -10,12 +13,14 @@ PLEA_FORMS = [
     ("about", forms.AboutForm),
     ("plea", forms.PleaInfoForm),
     ("review", forms.ReviewForm),
+    ("review_send_error", forms.ReviewForm)
 ]
 
 PLEA_TEMPLATES = {
     "about": "plea/about.html",
     "plea": "plea/plea.html",
     "review": "plea/review.html",
+    "review_send_error": "plea/review_send_error.html"
 }
 
 
@@ -25,7 +30,7 @@ class PleaForms(NamedUrlSessionWizardView):
 
     def get_context_data(self, form, **kwargs):
         context = super(PleaForms, self).get_context_data(form=form, **kwargs)
-        if self.steps.current == 'review':
+        if self.steps.current in ['review', 'review_send_error']:
             context.update({'all_data': self.get_all_cleaned_data()})
         
         if 'next' in self.request.GET:
@@ -40,6 +45,25 @@ class PleaForms(NamedUrlSessionWizardView):
                 next_url = self.get_step_url(self.request.GET['next'])
                 return HttpResponseRedirect(next_url)
         return next_form
+
+    def render_done(self, form, **kwargs):
+        final_form_list = []
+        # walk through the form list and try to validate the data again.
+        for form_key in self.get_form_list():
+            form_obj = self.get_form(step=form_key,
+                                     data=self.storage.get_step_data(form_key),
+                                     files=self.storage.get_step_files(form_key))
+            if not form_obj.is_valid():
+                return self.render_revalidation_failure(form_key, form_obj, **kwargs)
+            final_form_list.append(form_obj)
+
+        try:
+            done_response = self.done(final_form_list, **kwargs)
+        except (smtplib.SMTPException, socket.error, socket.gaierror) as e:
+            return HttpResponseRedirect(reverse_lazy('plea_form_step', args=('review_send_error', )))
+        else:
+            self.storage.reset()
+            return done_response
     
     def done(self, form_list, **kwargs):
         # Send the email
@@ -55,4 +79,3 @@ class PleaForms(NamedUrlSessionWizardView):
 
 class CompleteStep(TemplateView):
     template_name = "plea/complete.html"
-
--- manchester_traffic_offences/urls.py
+++ manchester_traffic_offences/urls.py
@@ -4,25 +4,20 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-
 from django.contrib import admin
-admin.autodiscover()
-
 from django.views.generic import TemplateView
 
 from apps.defendant.views import DefendantLogin
-from apps.defendant.decorators import urn_required
+
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    
-    url(r'^$', TemplateView.as_view(template_name="start.html")),
-    
-    url(r'^urn/', DefendantLogin.as_view(), name="urn"),
-    
-    url(r'^plea/', include('plea.urls',)),
-    
-    url(r'^feedback/', TemplateView.as_view(
-        template_name="feedback.html"), 
-        name="feedback"),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', TemplateView.as_view(template_name="start.html")),
+                       url(r'^urn/', DefendantLogin.as_view(), name="urn"),
+                       url(r'^plea/', include('plea.urls', )),
+                       url(r'^feedback/', TemplateView.as_view(
+                           template_name="feedback.html"),
+                           name="feedback"),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
