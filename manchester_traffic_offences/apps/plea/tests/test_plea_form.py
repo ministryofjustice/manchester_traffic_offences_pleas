16c9ed4e546a8cf4cd0315d3344c5b7d98d22724
Ian George <ian.george@digital.justice.gov.uk>
2014-07-03 16:17:13 +0100

    Form / view changed to new system and tests added

--- manchester_traffic_offences/apps/plea/tests/test_plea_form.py
+++ manchester_traffic_offences/apps/plea/tests/test_plea_form.py
@@ -1,6 +1,12 @@
+import datetime
+from mock import patch
+
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 from plea.forms import AboutForm
+from plea.views import PleaOnlineForms
+
 
 about_data_valid = {
     'date_of_hearing': '01/01/1977',
@@ -17,6 +23,52 @@ about_data_invalid = {
 }
 
 
+class TestMultiPleaForms(TestCase):
+    def test_successful_completion_single_charge(self):
+        fake_session = {}
+        request_context = {}
+
+        form = PleaOnlineForms("about", "plea_form_step", fake_session)
+        response = form.load(request_context)
+        response = form.save({"date_of_hearing": "2015-01-01",
+                              "urn": "00/AA/0000000/00",
+                              "name": "Charlie Brown",
+                              "number_of_charges": "2"},
+                             request_context)
+
+        self.assertEqual(response.status_code, 302)
+
+        form = PleaOnlineForms("plea", "plea_form_step", fake_session)
+        response = form.load(request_context)
+        response = form.save({"guilty": "guilty",
+                              "mitigations": "lorem ipsum",
+                              "understand": "True"},
+                             request_context)
+
+        self.assertEqual(response.status_code, 302)
+
+        form = PleaOnlineForms("review", "plea_form_step", fake_session)
+        response = form.load(request_context)
+        response = form.save({},
+                             request_context)
+
+        self.assertEqual(response.status_code, 302)
+
+        form = PleaOnlineForms("complete", "plea_form_step", fake_session)
+        response = form.load(request_context)
+
+        self.assertEqual(fake_session["date_of_hearing"], datetime.date(2015, 1, 1))
+        self.assertEqual(fake_session["urn"], "00/AA/0000000/00")
+        self.assertEqual(fake_session["name"], "Charlie Brown")
+        self.assertEqual(fake_session["number_of_charges"], 2)
+        self.assertEqual(fake_session["guilty"], "guilty")
+        self.assertEqual(fake_session["mitigations"], "lorem ipsum")
+        self.assertEqual(fake_session["understand"], True)
+
+    def successful_completion_multiple_charges(self):
+        pass
+
+
 class TestPleaFormWizzad(TestCase):
     
     def test_about_form_valid(self):
@@ -31,3 +83,5 @@ class TestPleaFormWizzad(TestCase):
         self.assertTrue('date_of_hearing' in f.errors)
         self.assertTrue('name' in f.errors)
         self.assertTrue('urn' in f.errors)
+
+
