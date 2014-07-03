12c8471a97c907558d166a8dd37eb44d783b2167
Ian George <ian.george@digital.justice.gov.uk>
2014-07-03 16:17:13 +0100

    Form / view changed to new system and tests added

--- manchester_traffic_offences/apps/plea/views.py
+++ manchester_traffic_offences/apps/plea/views.py
@@ -1,27 +1,78 @@
 import smtplib
 import socket
 
+from django.contrib.formtools.wizard.views import NamedUrlSessionWizardView
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
+from django.shortcuts import RequestContext
 from django.views.generic import TemplateView
-from django.contrib.formtools.wizard.views import NamedUrlSessionWizardView
 
 from . import forms
+from govuk_utils.forms import FormStage, MultiStageForm
 from email import send_plea_email
 
-PLEA_FORMS = [
-    ("about", forms.AboutForm),
-    ("plea", forms.PleaInfoForm),
-    ("review", forms.ReviewForm),
-    ("review_send_error", forms.ReviewForm)
-]
 
-PLEA_TEMPLATES = {
-    "about": "plea/about.html",
-    "plea": "plea/plea.html",
-    "review": "plea/review.html",
-    "review_send_error": "plea/review_send_error.html"
-}
+class AboutStage(FormStage):
+    name = "about"
+    template = "plea/about.html"
+    form_classes = [forms.AboutForm, ]
+
+
+class PleaStage(FormStage):
+    name = "plea"
+    template = "plea/plea.html"
+    form_classes = [forms.PleaInfoForm, ]
+
+
+class ReviewStage(FormStage):
+    name = "review"
+    template = "plea/review.html"
+    form_classes = []
+
+    def save(self, form_data):
+        response = super(ReviewStage, self).save(form_data)
+
+        try:
+            send_plea_email(self.form_data)
+            next_step = reverse_lazy("plea_form_step", args=("complete", ))
+        except (smtplib.SMTPException, socket.error, socket.gaierror) as e:
+            next_step = reverse_lazy('plea_form_step', args=('review_send_error', ))
+
+        self.next = next_step
+        return form_data
+
+
+class ReviewSendErrorStage(FormStage):
+    name = "send_error"
+    template = "plea/review_send_error.html"
+    form_classes = []
+
+
+class CompleteStage(FormStage):
+    name = "complete"
+    template = "plea/complete.html"
+    form_classes = []
+
+
+class PleaOnlineForms(MultiStageForm):
+    stage_classes = [AboutStage,
+                     PleaStage,
+                     ReviewStage,
+                     ReviewSendErrorStage,
+                     CompleteStage]
+
+
+class PleaOnlineViews(TemplateView):
+    def get(self, request, stage):
+        form = PleaOnlineForms(stage, "plea_form_step", request.session)
+        return form.load(RequestContext(request))
+
+    def post(self, request, stage):
+        form = PleaOnlineForms(stage, "plea_form_step", request.session)
+        return form.save(request.POST, RequestContext(request))
+
+
+
 
 
 class PleaForms(NamedUrlSessionWizardView):
