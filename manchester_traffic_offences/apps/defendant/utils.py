4dd0cbd9276ac698c37d9a8b973d1d61db5ed7b6
Sym Roe <sym.roe@talusdesign.co.uk>
2014-07-06 09:34:56 +0100

    [#74375308] better docstring

--- manchester_traffic_offences/apps/defendant/utils.py
+++ manchester_traffic_offences/apps/defendant/utils.py
@@ -5,12 +5,14 @@ from django.utils.translation import ugettext_lazy as _
 
 def is_valid_urn_format(urn):
     """
-    URN is 13 characters long in the following format:
+    URN is 11 or 13 characters long in the following format:
     
     00/AA/0000000/00
+    or
+    00/AA/00000/00
     """
 
-    pattern = r"[0-9]{2}/[a-zA-Z]{2}/(?:[0-9]{7}|[0-9]{5})/[0-9]{2}"
+    pattern = r"[0-9]{2}/[a-zA-Z]{2}/(?:[0-9]{5}|[0-9]{7})/[0-9]{2}"
 
     if not re.match(pattern, urn):
         raise exceptions.ValidationError(_('Not a valid URN'))
