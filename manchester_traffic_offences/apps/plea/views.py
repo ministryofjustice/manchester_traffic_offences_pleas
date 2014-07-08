fc1bba40f8c9154cf1715dfdafe30396d09d1ea4
Ian George <ian.george@digital.justice.gov.uk>
2014-07-08 14:24:33 +0100

    Framework updates, multiple plea step, email update.

--- manchester_traffic_offences/apps/plea/views.py
+++ manchester_traffic_offences/apps/plea/views.py
@@ -1,9 +1,8 @@
 import smtplib
 import socket
 
-from django.contrib.formtools.wizard.views import NamedUrlSessionWizardView
 from django.core.urlresolvers import reverse_lazy
-from django.http import HttpResponseRedirect
+from django.forms.formsets import formset_factory
 from django.shortcuts import RequestContext
 from django.views.generic import TemplateView
 
@@ -21,7 +20,31 @@ class AboutStage(FormStage):
 class PleaStage(FormStage):
     name = "plea"
     template = "plea/plea.html"
-    form_classes = [forms.PleaInfoForm, ]
+    form_classes = [forms.PleaInfoForm, forms.PleaForm]
+
+    def load_forms(self, data=None, initial=False):
+        count = self.all_data["about"].get("number_of_charges", 1)
+
+        PleaForms = formset_factory(forms.PleaForm, extra=count)
+        if initial:
+            initial_plea_data = self.all_data[self.name].get("PleaForms", [])
+            initial_info_data = self.all_data[self.name]
+            self.forms.append(forms.PleaInfoForm(initial=initial_info_data))
+            self.forms.append(PleaForms(initial=initial_plea_data))
+        else:
+            self.forms.append(forms.PleaInfoForm(data))
+            self.forms.append(PleaForms(data))
+
+    def save_forms(self):
+        form_data = {}
+
+        for form in self.forms:
+            if hasattr(form, "management_form"):
+                form_data["PleaForms"] = form.cleaned_data
+            else:
+                form_data.update(form.cleaned_data)
+
+        return form_data
 
 
 class ReviewStage(FormStage):
@@ -33,7 +56,7 @@ class ReviewStage(FormStage):
         response = super(ReviewStage, self).save(form_data)
 
         try:
-            send_plea_email(self.form_data)
+            send_plea_email(self.all_data)
             next_step = reverse_lazy("plea_form_step", args=("complete", ))
         except (smtplib.SMTPException, socket.error, socket.gaierror) as e:
             next_step = reverse_lazy('plea_form_step', args=('review_send_error', ))
@@ -64,70 +87,9 @@ class PleaOnlineForms(MultiStageForm):
 
 class PleaOnlineViews(TemplateView):
     def get(self, request, stage):
-        print {k: v for k, v in request.session.items()}
         form = PleaOnlineForms(stage, "plea_form_step", request.session)
         return form.load(RequestContext(request))
 
     def post(self, request, stage):
         form = PleaOnlineForms(stage, "plea_form_step", request.session)
         return form.save(request.POST, RequestContext(request))
-
-
-
-
-
-class PleaForms(NamedUrlSessionWizardView):
-    def get_template_names(self):
-        return [PLEA_TEMPLATES[self.steps.current]]
-
-    def get_context_data(self, form, **kwargs):
-        context = super(PleaForms, self).get_context_data(form=form, **kwargs)
-        if self.steps.current in ['review', 'review_send_error']:
-            context.update({'all_data': self.get_all_cleaned_data()})
-        
-        if 'next' in self.request.GET:
-            context.update({'next': self.request.GET['next']})
-    
-        return context
-    
-    def post(self, *args, **kwargs):
-        next_form = super(PleaForms, self).post(*args, **kwargs)
-        if 'next' in self.request.POST:
-            if self.request.POST['next'] in self.get_form_list():
-                next_url = self.get_step_url(self.request.GET['next'])
-                return HttpResponseRedirect(next_url)
-        return next_form
-
-    def render_done(self, form, **kwargs):
-        final_form_list = []
-        # walk through the form list and try to validate the data again.
-        for form_key in self.get_form_list():
-            form_obj = self.get_form(step=form_key,
-                                     data=self.storage.get_step_data(form_key),
-                                     files=self.storage.get_step_files(form_key))
-            if not form_obj.is_valid():
-                return self.render_revalidation_failure(form_key, form_obj, **kwargs)
-            final_form_list.append(form_obj)
-
-        try:
-            done_response = self.done(final_form_list, **kwargs)
-        except (smtplib.SMTPException, socket.error, socket.gaierror) as e:
-            return HttpResponseRedirect(reverse_lazy('plea_form_step', args=('review_send_error', )))
-
-        self.storage.reset()
-        return done_response
-    
-    def done(self, form_list, **kwargs):
-        # Send the email
-        context_data = {}
-
-        for form in form_list:
-            context_data.update(form.cleaned_data)
-
-        send_plea_email(context_data)
-
-        return HttpResponseRedirect(reverse_lazy("complete_step"))
-
-
-class CompleteStep(TemplateView):
-    template_name = "plea/complete.html"
