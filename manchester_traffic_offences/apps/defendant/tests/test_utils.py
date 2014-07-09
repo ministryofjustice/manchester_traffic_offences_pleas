c85c1f1275bc2e053e8e0a2e535b9124cde98627
Sym Roe <sym.roe@talusdesign.co.uk>
2014-07-09 12:48:49 +0100

    [#74375308] Test for new URN styles

--- manchester_traffic_offences/apps/defendant/tests/test_utils.py
+++ manchester_traffic_offences/apps/defendant/tests/test_utils.py
@@ -5,15 +5,15 @@ from defendant.utils import is_valid_urn_format
 
 class UtilsTestCase(TestCase):
     urls = 'defendant.tests.urls'
-    
+
     def test_is_valid_urn_format(self):
-        good_URNs = [
+        good_urns = [
             "00/AA/0000000/00",
             "12/bb/1234567/12",
             "06/JJ/50563/14",
             "06/JJ/50534/14",
         ]
-        bad_URNs = [
+        bad_urns = [
             "123",
             "00bb/0000000/00",
             "AA/bb/0000000/00",
@@ -21,10 +21,10 @@ class UtilsTestCase(TestCase):
             "0/bb/0000000/00",
             "00/bb/0000000/0",
         ]
-        
-        for URN in good_URNs:
+
+        for URN in good_urns:
             self.assertTrue(is_valid_urn_format(URN))
-        
-        for URN in bad_URNs:
+
+        for URN in bad_urns:
             with self.assertRaises(forms.ValidationError):
                 is_valid_urn_format(URN)
