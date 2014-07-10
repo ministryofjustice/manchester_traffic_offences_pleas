357db174b0273536f35e400e21589d9b7a8c0396
Ian George <ian.george@digital.justice.gov.uk>
2014-07-10 10:31:26 +0100

    Fixing date / datetime problem with new json parser

--- manchester_traffic_offences/apps/govuk_utils/tests.py
+++ manchester_traffic_offences/apps/govuk_utils/tests.py
@@ -224,17 +224,17 @@ class DateAwareSerializerTests(TestCase):
         self.assertEqual(ds, test_dict)
 
     def test_serializer_dumps_datetime(self):
-        test_str = """{"key2": "2014-06-03", "key1": "value1"}"""
+        test_str = """{"key2": "2014-06-03T00:00:00", "key1": "value1"}"""
         test_dict = {"key1": "value1",
-                     "key2": datetime(2014, 6, 3)}
+                     "key2": datetime(2014, 6, 3, 0, 0)}
 
         ds = DateAwareSerializer().dumps(test_dict)
         self.assertEqual(ds, test_str)
 
     def test_serializer_loads_datetime(self):
-        test_str = """{"key2": "2014-06-03", "key1": "value1"}"""
+        test_str = """{"key2": "2014-06-03T00:00:00", "key1": "value1"}"""
         test_dict = {u"key1": u"value1",
-                     u"key2": date(2014, 6, 3)}
+                     u"key2": datetime(2014, 6, 3, 0, 0)}
 
         ds = DateAwareSerializer().loads(test_str)
         self.assertEqual(ds, test_dict)
