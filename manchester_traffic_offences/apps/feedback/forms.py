f3b871d83372c413c117e52c766f7b29d68586f1
Ian George <ian.george@digital.justice.gov.uk>
2014-07-29 16:29:58 +0100

    [#75793182] Form to email and redirect / success messaging working

--- manchester_traffic_offences/apps/feedback/forms.py
+++ manchester_traffic_offences/apps/feedback/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class FeedbackForm(forms.Form):
-    email = forms.EmailField(required=False)
-    feedback = forms.CharField(widget=forms.TextInput)
+    email = forms.EmailField(required=False, label="Your email address")
+    question = forms.CharField(widget=forms.Textarea(attrs={"rows": 5, "cols": 50}), label="Your question")
