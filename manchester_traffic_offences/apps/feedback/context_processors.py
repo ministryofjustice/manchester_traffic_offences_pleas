f4103fa5d55399526c19da9f9cab514eb86a33d8
Ian George <ian.george@digital.justice.gov.uk>
2014-07-30 14:39:09 +0100

    [#75793182] Making sure the user returns to the page they were on, adding elements to the data capture for the email, and making things look a bit nicer

---
+++ manchester_traffic_offences/apps/feedback/context_processors.py
@@ -0,0 +1,18 @@
+from django.core.urlresolvers import reverse
+
+
+def feedback(request):
+    feedback_url = reverse("feedback_form")
+    #using url names so that people can't just put arbitrary urls in the next field
+    url_name = request.resolver_match.url_name
+    url_kwargs = request.resolver_match.kwargs
+    if url_kwargs:
+        url_kwargs = "&".join(["{0}={1}".format(k, v) for k, v in url_kwargs.items()])
+        full_url = "{0}?next={1}&{2}".format(feedback_url, url_name, url_kwargs)
+    else:
+        full_url = "{0}?next={1}".format(feedback_url, url_name)
+
+    return {
+        # Feedback URL (URL for feedback link in phase banner)
+        'feedback_url': full_url
+    }
