9e40e1473fc2d7294926bc2b5f768376d465114b
Ian George <ian.george@digital.justice.gov.uk>
2014-07-30 14:39:09 +0100

    [#75793182] Making sure the user returns to the page they were on, adding elements to the data capture for the email, and making things look a bit nicer

--- manchester_traffic_offences/apps/feedback/views.py
+++ manchester_traffic_offences/apps/feedback/views.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from django.contrib import messages
 from django.core.mail import EmailMessage
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
@@ -10,24 +11,30 @@ from .forms import FeedbackForm
 
 
 def feedback_form(request):
-    nxt = request.GET.get("next", "/")
-
+    kw_args = {k: v for (k, v) in request.GET.items()}
+    nxt = kw_args.pop("next", "/")
+    if kw_args:
+        nxt_url = reverse(nxt, kwargs=kw_args)
+    else:
+        nxt_url = reverse(nxt)
     if request.method == "POST":
         feedback_form = FeedbackForm(request.POST)
         if feedback_form.is_valid():
             email_context = {"question": feedback_form.cleaned_data["question"],
                              "email": feedback_form.cleaned_data["email"],
-                             "date_sent": datetime.now()}
+                             "date_sent": datetime.now(),
+                             "referrer": nxt_url,
+                             "user_agent": request.META["HTTP_USER_AGENT"]}
 
             email = EmailMessage("Feedback from makeaplea.justice.gov.uk",
-                                 render_to_string("feedback/feedback_email.html",
+                                 render_to_string("feedback/email.html",
                                                   email_context),
                                  "ian.george@digital.justice.gov.uk",
                                  ("ian.george@digital.justice.gov.uk", ))
             email.content_subtype = "html"
             email.send()
             messages.add_message(request, messages.INFO, "Thanks for your feedback.")
-            return HttpResponseRedirect(nxt)
+            return HttpResponseRedirect(nxt_url)
         else:
             return render(request, "feedback/feedback.html", {"form": feedback_form})
     else:
