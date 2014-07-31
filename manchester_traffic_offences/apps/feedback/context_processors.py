df26b11ad09000a91225d9b9467d65847c5adeb7
Ian George <ian.george@digital.justice.gov.uk>
2014-07-31 14:23:03 +0100

    [#75793182] Fixing broken tests

--- manchester_traffic_offences/apps/feedback/context_processors.py
+++ manchester_traffic_offences/apps/feedback/context_processors.py
@@ -6,6 +6,8 @@ def feedback(request):
     #using url names so that people can't just put arbitrary urls in the next field
     url_name = request.resolver_match.url_name
     url_kwargs = request.resolver_match.kwargs
+
+
     if url_kwargs:
         url_kwargs = "&".join(["{0}={1}".format(k, v) for k, v in url_kwargs.items()])
         full_url = "{0}?next={1}&{2}".format(feedback_url, url_name, url_kwargs)
