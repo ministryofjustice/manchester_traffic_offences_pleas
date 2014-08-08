7c11ded1f8ac2c026d31ad9780433c8f645e46a8
Ian George <ian.george@digital.justice.gov.uk>
2014-08-08 14:58:37 +0100

    [#76586676] Covered the case where request.resolver_match == None in the feedback context processor

--- manchester_traffic_offences/apps/feedback/context_processors.py
+++ manchester_traffic_offences/apps/feedback/context_processors.py
@@ -4,9 +4,12 @@ from django.core.urlresolvers import reverse
 def feedback(request):
     feedback_url = reverse("feedback_form")
     #using url names so that people can't just put arbitrary urls in the next field
-    url_name = request.resolver_match.url_name
-    url_kwargs = request.resolver_match.kwargs
-
+    if request.resolver_match is not None:
+        url_name = request.resolver_match.url_name
+        url_kwargs = request.resolver_match.kwargs
+    else:
+        url_name = "home"
+        url_kwargs = {}
 
     if url_kwargs:
         url_kwargs = "&".join(["{0}={1}".format(k, v) for k, v in url_kwargs.items()])
