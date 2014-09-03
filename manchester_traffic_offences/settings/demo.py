afe06eadd656f707adef9ac3dba7fa2cf88a2b3f
Ian George <ian.george@digital.justice.gov.uk>
2014-09-03 16:42:54 +0100

    Changing email settings back to amzn / makeaplea for demo

--- manchester_traffic_offences/settings/demo.py
+++ manchester_traffic_offences/settings/demo.py
@@ -31,14 +31,16 @@ ALLOWED_HOSTS = ["makeaplea.dsd.io", ]
 SESSION_COOKIE_SECURE = True
 
 # Emails
-SMTP_ROUTES["GSI"]["HOST"] = os.environ.get('GSI_EMAIL_HOST', '')
-SMTP_ROUTES["GSI"]["PORT"] = int(os.environ.get('GSI_EMAIL_PORT', '25'))
-
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'email-smtp.eu-west-1.amazonaws.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USERNAME']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_USE_TLS = True
+
+SMTP_ROUTES["GSI"]["HOST"] = EMAIL_HOST
+SMTP_ROUTES["GSI"]["PORT"] = EMAIL_PORT
+SMTP_ROUTES["GSI"]["USERNAME"] = EMAIL_HOST_USER
+SMTP_ROUTES["GSI"]["PASSWORD"] = EMAIL_HOST_PASSWORD
 
 PLEA_EMAIL_FROM = os.environ['PLEA_EMAIL_FROM']
 PLEA_EMAIL_TO = [os.environ['PLEA_EMAIL_TO'], ]
