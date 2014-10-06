a9fac34807d512e59d3af70783b0170a37ad13b3
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2014-10-06 18:37:40 +0100

    [MAPDEV13] user emails initial dev work

--- apps/plea/tests/test_email_generation.py
+++ apps/plea/tests/test_email_generation.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import
-import unittest
 
+from django.test import TestCase
 from django.core import mail
 
 from ..email import TemplateAttachmentEmail, send_plea_email
 
 
-class EmailGenerationTests(unittest.TestCase):
+class EmailGenerationTests(TestCase):
     def setUp(self):
         mail.outbox = []
 
@@ -17,12 +17,14 @@ class EmailGenerationTests(unittest.TestCase):
                                         "plea/plea_email_attachment.html",
                                         email_context,
                                         "<p>Test Content</p><br><p>{{ URN }}</p>")
+
         email.send(["test_to@example.org", ],
                    "Subject line",
                    "Body Text")
 
         self.assertEqual(mail.outbox[0].subject, "Subject line")
         self.assertEqual(mail.outbox[0].body, "Body Text")
+
 
     def test_plea_email_sends(self):
         context_data = {"case": {"date_of_hearing": "2014-06-30",
@@ -33,7 +35,24 @@ class EmailGenerationTests(unittest.TestCase):
                                                {"mitigations": "test2", "guilty": "guilty"}],
                                  "understand": True}}
 
-        send_plea_email(context_data)
+        with self.settings(SEND_PLEA_CONFIRMATION_EMAIL=False):
+            send_plea_email(context_data)
 
         # only expecting 1 email for now, PLP email cancelled for the time being
         self.assertEqual(len(mail.outbox), 2)
+
+    def test_user_confirmation_sends_email(self):
+        context_data = {"case": {"date_of_hearing": "2014-06-30",
+                                 "time_of_hearing": "12:00:00",
+                                 "urn": "cvxcvx89"},
+                        "your_details": {"name": "vcx", "email": "lyndon@antlyn.com"},
+                        "plea": {"PleaForms": [{"mitigations": "test1", "guilty": "guilty"},
+                                               {"mitigations": "test2", "guilty": "guilty"}],
+                                 "understand": True}}
+
+        with self.settings(SEND_PLEA_CONFIRMATION_EMAIL=True, PLEA_CONFIRMATION_EMAIL_BCC=[]):
+            send_plea_email(context_data)
+
+        self.assertEqual(len(mail.outbox), 3)
+        self.assertIn(context_data['case']['urn'], mail.outbox[-1].body)
+        self.assertIn(context_data['your_details']['email'], mail.outbox[-1].to)
