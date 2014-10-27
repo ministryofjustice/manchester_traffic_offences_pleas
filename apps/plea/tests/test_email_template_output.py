8a861c3e603bcea9e43663170841906724b32fac
Ian George <ian.george@digital.justice.gov.uk>
2014-10-27 15:05:02 +0000

    Added unit tests for email output
    The email template output, whilst not strictly the best thing to be
    unit testing, is an important part of the application so for the most
    important pieces of info there is now a unit test to ensure that what
    is put into the email send is present and correct in the attachment
    generated.
    
    [https://makeaplea.leankit.com/Boards/View/126993392/137753822]

--- apps/plea/tests/test_email_template_output.py
+++ apps/plea/tests/test_email_template_output.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 from django.test import TestCase
 from django.core import mail
 from mock import Mock
@@ -16,21 +18,19 @@ class EmailTemplateTests(TestCase):
                                  "driving_licence_number": "xxx", "registration_number": "xxx"},
                 "plea": {"PleaForms": [{"mitigations": "test1", "guilty": "guilty"}],
                          "understand": True},
-                "your_money": {
+                "your_money": {}}
 
-                }}
-
-    def get_fake_response(self, html):
+    def get_mock_response(self, html):
         response = Mock()
         response.status_code = 200
-        response.content = html
+        response.content = html.encode("utf-8")
         response.__str__ = Mock()
-        response.__str__.return_value = html
-        response._charset = "UTF-8"
+        response.__str__.return_value = html.encode("utf-8")
+        response._charset = "utf-8"
         response.streaming = False
         return response
 
-    def test_single_guilty_plea_email_output(self):
+    def test_single_guilty_plea_email_plea_output(self):
         context_data = self.get_context_data()
         with self.settings(SEND_PLEA_CONFIRMATION_EMAIL=False):
             send_plea_email(context_data)
@@ -39,12 +39,11 @@ class EmailTemplateTests(TestCase):
         self.assertEqual(len(mail.outbox), 2)
         self.assertEqual(mail.outbox[0].subject, 'ONLINE PLEA: 00/AA/00000/00 DOH: 2015-10-30 GEORGE Ian')
         self.assertEqual(len(mail.outbox[0].attachments), 1)
-        self.assertEqual(mail.outbox[0].body, "<<<makeaplea-ref: 12/7>>>")
 
-        response = self.get_fake_response(mail.outbox[0].attachments[0][1])
+        response = self.get_mock_response(mail.outbox[0].attachments[0][1])
         self.assertContains(response, "<tr><th>Plea</th><td>Guilty</td></tr>", count=1, html=True)
 
-    def test_multiple_guilty_plea_email_output(self):
+    def test_multiple_guilty_plea_email_plea_output(self):
         context_data = self.get_context_data()
 
         context_data["plea"]["PleaForms"].append({"mitigations": "test2", "guilty": "guilty"})
@@ -56,12 +55,11 @@ class EmailTemplateTests(TestCase):
         self.assertEqual(len(mail.outbox), 2)
         self.assertEqual(mail.outbox[0].subject, 'ONLINE PLEA: 00/AA/00000/00 DOH: 2015-10-30 GEORGE Ian')
         self.assertEqual(len(mail.outbox[0].attachments), 1)
-        self.assertEqual(mail.outbox[0].body, "<<<makeaplea-ref: 10/5>>>")
 
-        response = self.get_fake_response(mail.outbox[0].attachments[0][1])
+        response = self.get_mock_response(mail.outbox[0].attachments[0][1])
         self.assertContains(response, "<tr><th>Plea</th><td>Guilty</td></tr>", count=2, html=True)
 
-    def test_single_not_guilty_plea_email_output(self):
+    def test_single_not_guilty_plea_email_plea_output(self):
         context_data = self.get_context_data()
         context_data["plea"]["PleaForms"][0]["guilty"] = "not_guilty"
 
@@ -72,12 +70,11 @@ class EmailTemplateTests(TestCase):
         self.assertEqual(len(mail.outbox), 2)
         self.assertEqual(mail.outbox[0].subject, 'ONLINE PLEA: 00/AA/00000/00 DOH: 2015-10-30 GEORGE Ian')
         self.assertEqual(len(mail.outbox[0].attachments), 1)
-        self.assertEqual(mail.outbox[0].body, "<<<makeaplea-ref: 13/8>>>")
 
-        response = self.get_fake_response(mail.outbox[0].attachments[0][1])
+        response = self.get_mock_response(mail.outbox[0].attachments[0][1])
         self.assertContains(response, "<tr><th>Plea</th><td>Not guilty</td></tr>", count=1, html=True)
 
-    def test_multiple_not_guilty_plea_email_output(self):
+    def test_multiple_not_guilty_plea_email_plea_output(self):
         context_data = self.get_context_data()
         context_data["plea"]["PleaForms"][0]["guilty"] = "not_guilty"
         context_data["plea"]["PleaForms"].append({"mitigations": "test2", "guilty": "not_guilty"})
@@ -89,12 +86,11 @@ class EmailTemplateTests(TestCase):
         self.assertEqual(len(mail.outbox), 2)
         self.assertEqual(mail.outbox[0].subject, 'ONLINE PLEA: 00/AA/00000/00 DOH: 2015-10-30 GEORGE Ian')
         self.assertEqual(len(mail.outbox[0].attachments), 1)
-        self.assertEqual(mail.outbox[0].body, "<<<makeaplea-ref: 11/6>>>")
 
-        response = self.get_fake_response(mail.outbox[0].attachments[0][1])
+        response = self.get_mock_response(mail.outbox[0].attachments[0][1])
         self.assertContains(response, "<tr><th>Plea</th><td>Not guilty</td></tr>", count=2, html=True)
 
-    def test_mixed_plea_email_output(self):
+    def test_mixed_plea_email_plea_output(self):
         context_data = self.get_context_data()
         context_data["plea"]["PleaForms"].append({"mitigations": "test2", "guilty": "not_guilty"})
 
@@ -105,8 +101,113 @@ class EmailTemplateTests(TestCase):
         self.assertEqual(len(mail.outbox), 2)
         self.assertEqual(mail.outbox[0].subject, 'ONLINE PLEA: 00/AA/00000/00 DOH: 2015-10-30 GEORGE Ian')
         self.assertEqual(len(mail.outbox[0].attachments), 1)
-        self.assertEqual(mail.outbox[0].body, "<<<makeaplea-ref: 9/4>>>")
 
-        response = self.get_fake_response(mail.outbox[0].attachments[0][1])
+        response = self.get_mock_response(mail.outbox[0].attachments[0][1])
         self.assertContains(response, "<tr><th>Plea</th><td>Guilty</td></tr>", count=1, html=True)
         self.assertContains(response, "<tr><th>Plea</th><td>Not guilty</td></tr>", count=1, html=True)
+
+    def test_employed_email_money_output(self):
+        context_data = self.get_context_data()
+        context_data["your_money"] = {"you_are": "Employed",
+                                      "employer_name": "Tesco",
+                                      "employer_address": "A big office\nSomewhere central\nAB12 123",
+                                      "employer_phone": "012374384",
+                                      "take_home_pay_period": "Weekly",
+                                      "take_home_pay_amount": "200",
+                                      }
+
+        with self.settings(SEND_PLEA_CONFIRMATION_EMAIL=False):
+            send_plea_email(context_data)
+
+        # only expecting 1 email for now, PLP email cancelled for the time being
+        self.assertEqual(len(mail.outbox), 2)
+        self.assertEqual(mail.outbox[0].subject, 'ONLINE PLEA: 00/AA/00000/00 DOH: 2015-10-30 GEORGE Ian')
+        self.assertEqual(len(mail.outbox[0].attachments), 1)
+
+        response = self.get_mock_response(mail.outbox[0].attachments[0][1])
+
+        self.assertContains(response, "<tr><th>Employer's name</th><td>Tesco</td></tr>", count=1, html=True)
+        self.assertContains(response, "<tr><th>Employer's full address</th><td>A big office<br />Somewhere central<br />AB12 123</td></tr>", count=1, html=True)
+        self.assertContains(response, "<tr><th>Employer's phone</th><td>012374384</td></tr>", count=1, html=True)
+        self.assertContains(response, "<tr><th>You get paid</th><td>Weekly</td></tr>", count=1, html=True)
+        self.assertContains(response, "<tr><th>Amount</th><td>£200</td></tr>", count=1, html=True)
+
+    def test_self_employed_email_money_output(self):
+        context_data = self.get_context_data()
+        context_data["your_money"] = {"you_are": "Self employed",
+                                      "your_job": "Tesco",
+                                      "self_employed_pay_period": "Weekly",
+                                      "self_employed_pay_amount": "200",
+                                      }
+
+        with self.settings(SEND_PLEA_CONFIRMATION_EMAIL=False):
+            send_plea_email(context_data)
+
+        # only expecting 1 email for now, PLP email cancelled for the time being
+        self.assertEqual(len(mail.outbox), 2)
+        self.assertEqual(mail.outbox[0].subject, 'ONLINE PLEA: 00/AA/00000/00 DOH: 2015-10-30 GEORGE Ian')
+        self.assertEqual(len(mail.outbox[0].attachments), 1)
+
+        response = self.get_mock_response(mail.outbox[0].attachments[0][1])
+
+        self.assertContains(response, "<tr><th>Your Job</th><td>Tesco</td></tr>", count=1, html=True)
+        self.assertContains(response, "<tr><th>You get paid</th><td>Weekly</td></tr>", count=1, html=True)
+        self.assertContains(response, "<tr><th>Amount</th><td>£200</td></tr>", count=1, html=True)
+
+    def test_self_employed_other_email_money_output(self):
+        context_data = self.get_context_data()
+        context_data["your_money"] = {"you_are": "Self employed",
+                                      "your_job": "Window cleaner",
+                                      "self_employed_pay_period": "self-employed other",
+                                      "self_employed_pay_amount": "20",
+                                      "self_employed_pay_other": "by the window"
+                                      }
+
+        with self.settings(SEND_PLEA_CONFIRMATION_EMAIL=False):
+            send_plea_email(context_data)
+
+        # only expecting 1 email for now, PLP email cancelled for the time being
+        self.assertEqual(len(mail.outbox), 2)
+        self.assertEqual(mail.outbox[0].subject, 'ONLINE PLEA: 00/AA/00000/00 DOH: 2015-10-30 GEORGE Ian')
+        self.assertEqual(len(mail.outbox[0].attachments), 1)
+
+        response = self.get_mock_response(mail.outbox[0].attachments[0][1])
+
+        self.assertContains(response, "<tr><th>Your Job</th><td>Window cleaner</td></tr>", count=1, html=True)
+        self.assertContains(response, "<tr><th>You get paid</th><td>Other - by the window</td></tr>", count=1, html=True)
+        self.assertContains(response, "<tr><th>Amount</th><td>£20</td></tr>", count=1, html=True)
+
+    def test_benefits_email_money_output(self):
+        context_data = self.get_context_data()
+        context_data["your_money"] = {"you_are": "Receiving benefits",
+                                      "benefits_period": "Weekly",
+                                      "benefits_amount": "120"}
+
+        with self.settings(SEND_PLEA_CONFIRMATION_EMAIL=False):
+            send_plea_email(context_data)
+
+        # only expecting 1 email for now, PLP email cancelled for the time being
+        self.assertEqual(len(mail.outbox), 2)
+        self.assertEqual(mail.outbox[0].subject, 'ONLINE PLEA: 00/AA/00000/00 DOH: 2015-10-30 GEORGE Ian')
+        self.assertEqual(len(mail.outbox[0].attachments), 1)
+
+        response = self.get_mock_response(mail.outbox[0].attachments[0][1])
+        self.assertContains(response, "<tr><th>You get paid</th><td>Weekly</td></tr>", count=1, html=True)
+        self.assertContains(response, "<tr><th>Amount</th><td>£120</td></tr>", count=1, html=True)
+
+    def test_other_email_money_output(self):
+        context_data = self.get_context_data()
+        context_data["your_money"] = {"you_are": "Other",
+                                      "other_info": "I am a pensioner and I earn\n£500 a month.",
+                                      "benefits_amount": "120"}
+
+        with self.settings(SEND_PLEA_CONFIRMATION_EMAIL=False):
+            send_plea_email(context_data)
+
+        # only expecting 1 email for now, PLP email cancelled for the time being
+        self.assertEqual(len(mail.outbox), 2)
+        self.assertEqual(mail.outbox[0].subject, 'ONLINE PLEA: 00/AA/00000/00 DOH: 2015-10-30 GEORGE Ian')
+        self.assertEqual(len(mail.outbox[0].attachments), 1)
+
+        response = self.get_mock_response(mail.outbox[0].attachments[0][1])
+        self.assertContains(response, "<tr><th>More information</th><td>I am a pensioner and I earn<br />£500 a month.</td></tr>", count=1, html=True)
