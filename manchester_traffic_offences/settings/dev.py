650ac5de2d80cb280d034cebf5547e06c71e6f04
Ian George <ian.george@digital.justice.gov.uk>
2014-11-05 15:16:23 +0000

    Settings for dev environment

--- manchester_traffic_offences/settings/dev.py
+++ manchester_traffic_offences/settings/dev.py
@@ -1,21 +1,49 @@
 from .base import *
+import os
 
-
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = ()
+INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
 
-MANAGERS = ADMINS
-
+RAVEN_CONFIG = {
+    'dsn': os.environ['RAVEN_DSN'],
+}
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pleaonline',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ.get('POSTGRES_PASS', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', ''),
+        'PORT': os.environ.get('POSTGRES_PORT', ''),
     }
 }
+
+ADMINS = (
+    ('Ian George', 'ian.george@digital.justice.gov.uk'),
+    ('Lyndon Garvey', 'lyndon.garvey@digital.justice.gov.uk')
+)
+
+MANAGERS = ADMINS
+ALLOWED_HOSTS = ["dev.makeaplea.dsd.io", ]
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'email-smtp.eu-west-1.amazonaws.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USERNAME']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_USE_TLS = True
+
+# Emails
+SMTP_ROUTES["GSI"]["HOST"] = EMAIL_HOST
+SMTP_ROUTES["GSI"]["PORT"] = EMAIL_PORT
+SMTP_ROUTES["GSI"]["USERNAME"] = EMAIL_HOST_USER
+SMTP_ROUTES["GSI"]["PASSWORD"] = EMAIL_HOST_PASSWORD
+
+PLEA_EMAIL_FROM = os.environ['PLEA_EMAIL_FROM']
+PLEA_EMAIL_TO = [os.environ['PLEA_EMAIL_TO'], ]
+PLP_EMAIL_TO = [os.environ["PLP_EMAIL_TO"], ]
+
+FEEDBACK_EMAIL_TO = [os.environ["FEEDBACK_EMAIL_TO"], ]
+FEEDBACK_EMAIL_FROM = os.environ["FEEDBACK_EMAIL_FROM"]
