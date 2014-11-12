2e5bd4f31f8b37dff4bde38d5236825d1090931f
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2014-11-12 13:13:18 +0000

    HTML Email confirmation of plea submission & outbound public email setup
    The outbound user submission confirmation email is now being routed
    through sendgrid which follows the set-up used by other teams for user
    facing email transmission.
    
    The full HTML email has been built and content amends made.
    
    https://makeaplea.leankit.com/Boards/View/126993392/143071996
    https://makeaplea.leankit.com/Boards/View/126993392/141181758

--- manchester_traffic_offences/settings/dev.py
+++ manchester_traffic_offences/settings/dev.py
@@ -7,14 +7,14 @@ TEMPLATE_DEBUG = DEBUG
 INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
 
 RAVEN_CONFIG = {
-    'dsn': os.environ['RAVEN_DSN'],
+    'dsn': os.environ.get('RAVEN_DSN', '')
 }
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['POSTGRES_DB'],
-        'USER': os.environ['POSTGRES_USER'],
+        'NAME': os.environ.get('POSTGRES_DB',''),
+        'USER': os.environ.get('POSTGRES_USER', ''),
         'PASSWORD': os.environ.get('POSTGRES_PASS', ''),
         'HOST': os.environ.get('POSTGRES_HOST', ''),
         'PORT': os.environ.get('POSTGRES_PORT', ''),
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = ["dev.makeaplea.dsd.io", ]
 
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'email-smtp.eu-west-1.amazonaws.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USERNAME']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USERNAME','')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = True
 
 # Emails
@@ -41,9 +41,9 @@ SMTP_ROUTES["GSI"]["PORT"] = EMAIL_PORT
 SMTP_ROUTES["GSI"]["USERNAME"] = EMAIL_HOST_USER
 SMTP_ROUTES["GSI"]["PASSWORD"] = EMAIL_HOST_PASSWORD
 
-PLEA_EMAIL_FROM = os.environ['PLEA_EMAIL_FROM']
-PLEA_EMAIL_TO = [os.environ['PLEA_EMAIL_TO'], ]
-PLP_EMAIL_TO = [os.environ["PLP_EMAIL_TO"], ]
+PLEA_EMAIL_FROM = os.environ.get('PLEA_EMAIL_FROM', '')
+PLEA_EMAIL_TO = [os.environ.get('PLEA_EMAIL_TO', '') ]
+PLP_EMAIL_TO = [os.environ.get("PLP_EMAIL_TO", '') ]
 
-FEEDBACK_EMAIL_TO = [os.environ["FEEDBACK_EMAIL_TO"], ]
-FEEDBACK_EMAIL_FROM = os.environ["FEEDBACK_EMAIL_FROM"]
+FEEDBACK_EMAIL_TO = [os.environ.get("FEEDBACK_EMAIL_TO", '')]
+FEEDBACK_EMAIL_FROM = os.environ.get("FEEDBACK_EMAIL_FROM", '')
