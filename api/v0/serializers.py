0f812b8595ec5ac0c0097f824e539e2d29b18701
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2014-11-25 14:38:06 +0000

    makeaplea secure API
    The URN validation regex was allowing random characters before and after
    the URN. This has now been fixed.

--- api/v0/serializers.py
+++ api/v0/serializers.py
@@ -6,7 +6,7 @@ from apps.plea.models import Case
 
 
 class CaseSerializer(serializers.ModelSerializer):
-    urn = serializers.RegexField("\d{2}/[a-zA-Z]{2}/\d+/\d{2}", max_length=16, min_length=14)
+    urn = serializers.RegexField("^\d{2}/[a-zA-Z]{2}/\d+/\d{2}$", max_length=16, min_length=14)
 
     class Meta:
         model = Case
