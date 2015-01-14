1ed5590e8d4cb1b3342a40f4100b845b23b7baf1
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2015-01-14 17:15:56 +0000

    changes made in response to code review

--- apps/plea/runner.py
+++ apps/plea/runner.py
@@ -10,16 +10,14 @@ from .encrypt import clear_user_data
 class MAPTestRunner(DiscoverRunner):
     def setup_test_environment(self, **kwargs):
 
-        if not os.path.isdir(settings.USER_DATA_DIRECTORY):
-            raise Exception("The user data directory directory {} does not exist"
-                            .format(settings.USER_DATA_DIRECTORY))
+        if not os.path.exists(settings.USER_DATA_DIRECTORY):
+            os.makedirs(settings.USER_DATA_DIRECTORY)
 
-        if not os.path.isdir(settings.GPG_HOME_DIRECTORY):
-            raise Exception("The gpg home directory {} does not exist"
-                            .format(settings.GPG_HOME_DIRECTORY))
+        if not os.path.exists(settings.GPG_HOME_DIRECTORY):
+            os.makedirs(settings.GPG_HOME_DIRECTORY)
 
         from .encrypt import gpg
-        import_result = gpg.import_keys(settings.GPG_TEST_KEY)
+        gpg.import_keys(settings.GPG_TEST_KEY)
 
         super(MAPTestRunner, self).setup_test_environment(**kwargs)
 
