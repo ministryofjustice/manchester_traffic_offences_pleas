ac7e30840950c4289c454f79f6be3b699c42dc80
Ian George <ian.george@digital.justice.gov.uk>
2015-01-19 14:36:14 +0000

    Using the base settings mostly for staging now.

--- manchester_traffic_offences/settings/staging.py
+++ manchester_traffic_offences/settings/staging.py
@@ -1,10 +1,9 @@
 from .base import *
 import os
 
+
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-
-INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
 
 DATABASES = {
     'default': {
@@ -17,27 +16,8 @@ DATABASES = {
     }
 }
 
+INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
 
 ALLOWED_HOSTS = ["staging.makeaplea.dsd.io", "makeaplea.dsd.io"]
 SESSION_COOKIE_SECURE = True
-
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'email-smtp.eu-west-1.amazonaws.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USERNAME']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-EMAIL_USE_TLS = True
-
-# Emails
-SMTP_ROUTES["GSI"]["HOST"] = EMAIL_HOST
-SMTP_ROUTES["GSI"]["PORT"] = EMAIL_PORT
-SMTP_ROUTES["GSI"]["USERNAME"] = EMAIL_HOST_USER
-SMTP_ROUTES["GSI"]["PASSWORD"] = EMAIL_HOST_PASSWORD
-
-PLEA_EMAIL_FROM = os.environ['PLEA_EMAIL_FROM']
-PLEA_EMAIL_TO = [os.environ['PLEA_EMAIL_TO'], ]
-PLP_EMAIL_TO = [os.environ["PLP_EMAIL_TO"], ]
-
-FEEDBACK_EMAIL_TO = [os.environ["FEEDBACK_EMAIL_TO"], ]
-FEEDBACK_EMAIL_FROM = os.environ["FEEDBACK_EMAIL_FROM"]
-
 STORE_USER_DATA = True
