cc6bee635f61c7bdd3b5277911906255a4b923f3
Ian George <ian.george@digital.justice.gov.uk>
2015-01-19 14:36:14 +0000

    Using the base settings mostly for staging now.

--- manchester_traffic_offences/settings/dev.py
+++ manchester_traffic_offences/settings/dev.py
@@ -1,14 +1,9 @@
 from .base import *
 import os
 
+
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-
-INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
-
-RAVEN_CONFIG = {
-    'dsn': os.environ.get('RAVEN_DSN', '')
-}
 
 DATABASES = {
     'default': {
@@ -20,5 +15,7 @@ DATABASES = {
         'PORT': os.environ.get('POSTGRES_PORT', ''),
     }
 }
+
+INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
 
 ALLOWED_HOSTS = ["dev.makeaplea.dsd.io", ]
