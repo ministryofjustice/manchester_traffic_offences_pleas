41e860df86165e537140bf73c0801dc8d5c7a742
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2015-01-23 14:51:26 +0000

    MAPDEDV104 - add graph data to the dashboard
    The following has been done to support the addition of a graph to the
    dashboard:
    
    New UsageStats model for storing weekly aggregate stats
    A management command to occupy the above table each week
    Extending the API to present the stats
    Unit tests updated accordingly

---
+++ apps/plea/migrations/0020_auto__add_usagestats.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'UsageStats'
+        db.create_table(u'plea_usagestats', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('start_date', self.gf('django.db.models.fields.DateTimeField')()),
+            ('submissions', self.gf('django.db.models.fields.PositiveIntegerField')()),
+            ('requisitions', self.gf('django.db.models.fields.PositiveIntegerField')(default=None, null=True, blank=True)),
+        ))
+        db.send_create_signal(u'plea', ['UsageStats'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'UsageStats'
+        db.delete_table(u'plea_usagestats')
+
+
+    models = {
+        u'plea.case': {
+            'Meta': {'object_name': 'Case'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'created_not_sent'", 'max_length': '30'}),
+            'status_info': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'urn': ('django.db.models.fields.CharField', [], {'max_length': '16', 'db_index': 'True'})
+        },
+        u'plea.courtemailcount': {
+            'Meta': {'object_name': 'CourtEmailCount'},
+            'date_sent': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'hearing_date': ('django.db.models.fields.DateTimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'sc_guilty_char_count': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'sc_not_guilty_char_count': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
+            'status_info': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'total_guilty': ('django.db.models.fields.IntegerField', [], {}),
+            'total_not_guilty': ('django.db.models.fields.IntegerField', [], {}),
+            'total_pleas': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'plea.usagestats': {
+            'Meta': {'object_name': 'UsageStats'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'requisitions': ('django.db.models.fields.PositiveIntegerField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
+            'start_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'submissions': ('django.db.models.fields.PositiveIntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['plea']
