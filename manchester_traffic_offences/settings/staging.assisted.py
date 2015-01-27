93b8f8bc73a24f7927b7764a884f0538025dca66
Ian George <ian.george@digital.justice.gov.uk>
2015-01-27 17:16:11 +0000

    Add assisted digital version to staging env
    The assisted digital path with the call centre now requires there to
    be a slightly altered version of the make a plea application.
    
    [MAPDEV127]

---
+++ manchester_traffic_offences/settings/staging.assisted.py
@@ -0,0 +1,26 @@
+from .base import *
+import os
+
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ.get('POSTGRES_PASS', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', ''),
+        'PORT': os.environ.get('POSTGRES_PORT', ''),
+    }
+}
+
+INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
+
+ALLOWED_HOSTS = ["staging-assisted.makeaplea.dsd.io", ]
+SESSION_COOKIE_SECURE = True
+STORE_USER_DATA = True
+
+SITE_ID = 2
+EMAIL_REQUIRED = False
