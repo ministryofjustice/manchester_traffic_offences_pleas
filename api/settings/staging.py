9f7344dba3c7a0ffaea48e7823b365fb676db38f
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2015-01-27 13:52:04 +0000

    added DB settings back into the api settings files

--- api/settings/staging.py
+++ api/settings/staging.py
@@ -5,4 +5,15 @@ TEMPLATE_DEBUG = DEBUG
 
 INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB',''),
+        'USER': os.environ.get('POSTGRES_USER', ''),
+        'PASSWORD': os.environ.get('POSTGRES_PASS', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', ''),
+        'PORT': os.environ.get('POSTGRES_PORT', ''),
+    }
+}
+
 ALLOWED_HOSTS = ["api.makeaplea.dsd.io", ]
