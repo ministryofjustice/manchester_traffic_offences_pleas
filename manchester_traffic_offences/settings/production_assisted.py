0883656bca0b1ada057442065ff3ed33a0ca73a3
Ian George <ian.george@digital.justice.gov.uk>
2015-01-27 21:32:09 +0000

    Added assisted digital settings for production

---
+++ manchester_traffic_offences/settings/production_assisted.py
@@ -0,0 +1,42 @@
+from .base import *
+import os
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ.get('POSTGRES_PASS', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', ''),
+        'PORT': os.environ.get('POSTGRES_PORT', ''),
+    }
+}
+
+
+ALLOWED_HOSTS = ["www.makeaplea.justice.gov.uk", ]
+SESSION_COOKIE_SECURE = True
+
+# Emails
+SMTP_ROUTES["GSI"]["HOST"] = os.environ.get('GSI_EMAIL_HOST', '')
+SMTP_ROUTES["GSI"]["PORT"] = int(os.environ.get('GSI_EMAIL_PORT', '25'))
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'email-smtp.eu-west-1.amazonaws.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USERNAME']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_USE_TLS = True
+
+PLEA_EMAIL_FROM = os.environ['PLEA_EMAIL_FROM']
+PLEA_EMAIL_TO = [os.environ['PLEA_EMAIL_TO'], ]
+PLP_EMAIL_TO = [os.environ["PLP_EMAIL_TO"], ]
+
+FEEDBACK_EMAIL_TO = [os.environ["FEEDBACK_EMAIL_TO"], ]
+FEEDBACK_EMAIL_FROM = os.environ["FEEDBACK_EMAIL_FROM"]
+
+STORE_USER_DATA = True
+EMAIL_REQUIRED = False
