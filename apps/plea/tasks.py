8854ddb205e04c10d5532eed2741b88c3bf64786
Ian George <ian.george@digital.justice.gov.uk>
2015-02-19 16:39:44 +0000

    Migrate inline statuses to an action list
    The inline status fields on the case table wasn't expressive enough to
    capture all of the actions of sending emails via celery tasks and
    processing receipts. We've moved to a more immediate set of boolean
    flags on the case and emailcount objects and also a list of actions
    on the case object.
    
    [MAPDEV167]

--- apps/plea/tasks.py
+++ apps/plea/tasks.py
@@ -27,6 +27,7 @@ def email_send_court(self, case_id, count_id, email_data):
     # No error trapping, let these fail hard if the objects can't be found
     case = Case.objects.get(pk=case_id)
     email_count = CourtEmailCount.objects.get(pk=count_id)
+    case.add_action("Court email started", "")
 
     email_body = "<<<makeaplea-ref: {}/{}>>>".format(case.id, email_count.id)
 
@@ -42,28 +43,30 @@ def email_send_court(self, case_id, count_id, email_data):
                         email_body,
                         route="GSI")
     except (smtplib.SMTPException, socket.error, socket.gaierror) as exc:
-        print "\nSent {0} - {1}\n".format(settings.PLEA_EMAIL_SUBJECT.format(**email_data), exc)
         logger.error("Error sending email to court: {0}".format(exc))
-
-        case.status = "network_error"
-        case.status_info = unicode(exc)
+        case.add_action("Court email network error", unicode(exc))
         email_count.get_status_from_case(case)
         email_count.save()
         case.save()
 
         raise self.retry(exc=exc)
 
-    case.status = "sent"
-    case.save()
+    case.add_action("Court email sent", "")
+
     email_count.get_status_from_case(case)
     email_count.save()
 
-    email_send_prosecutor.delay(email_data)
-    email_send_user.delay(email_data)
+    email_send_prosecutor.delay(email_data, case_id)
+    email_send_user.delay(email_data, case_id)
 
 
 @app.task(bind=True)
-def email_send_prosecutor(self, email_data):
+def email_send_prosecutor(self, email_data, case_id):
+
+    # No error trapping, let these fail hard if the objects can't be found
+    case = Case.objects.get(pk=case_id)
+    case.add_action("Prosecutor email started", "")
+
     plp_email_to = settings.PLP_EMAIL_TO
 
     plp_email = TemplateAttachmentEmail(settings.PLP_EMAIL_FROM,
@@ -79,17 +82,23 @@ def email_send_prosecutor(self, email_data):
                        route="GSI")
     except (smtplib.SMTPException, socket.error, socket.gaierror) as exc:
         logger.error("Error sending email to prosecutor: {0}".format(exc))
+        case.add_action("Prosecutor email network error", unicode(exc))
         raise self.retry(email_data, exc=exc)
+
+    case.add_action("Prosecutor email sent", "")
 
     return True
 
 
 @app.task(bind=True)
-def email_send_user(self, email_data):
+def email_send_user(self, email_data, case_id):
     """
     Dispatch an email to the user to confirm that their plea submission
     was successful.
     """
+    # No error trapping, let these fail hard if the objects can't be found
+    case = Case.objects.get(pk=case_id)
+    case.add_action("User email started", "")
 
     from .stages import get_plea_type
 
@@ -122,5 +131,9 @@ def email_send_user(self, email_data):
         email.send(fail_silently=False)
     except (smtplib.SMTPException, socket.error, socket.gaierror) as exc:
         logger.error("Error sending user confirmation email: {0}".format(exc))
+        case.add_action("Prosecutor email network error", unicode(exc))
         self.retry(email_data, exc=exc)
+
+    case.add_action("User email sent", "")
+
     return True
