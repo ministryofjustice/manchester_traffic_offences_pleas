230fbfe2838be8cc65a4be750b83db27dc661e98
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2015-03-03 16:33:49 +0000

    Add Satisfaction question to feedback form
    Copy and styles of Feedback screen updated. Satisfaction answer
    also added to resulting email.
    
    [MAPDEV166]

--- apps/feedback/forms.py
+++ apps/feedback/forms.py
@@ -1,9 +1,20 @@
 from django import forms
+from django.forms.formsets import BaseFormSet
+from django.forms.widgets import RadioSelect
+
+from apps.plea.fields import DSStackedRadioFieldRenderer
 
 class FeedbackForm(forms.Form):
-    feedback_email = forms.EmailField(label="Your email address",
+    SATISFACTION_CHOICES = (
+        ("very satisfied", "very satisfied"),
+        ("satisfied", "satisfied"),
+        ("neither satisfied nor dissatisfied", "neither satisfied nor dissatisfied"),
+        ("dissatisfied", "dissatisfied"),
+        ("very dissatisfied", "very dissatisfied"))
+
+    feedback_email = forms.EmailField(label="Email address",
     	                              required=False,
-    	                              help_text="We'll only use this to reply to your message.",
+    	                              help_text="If you'd like us to get back to you, please leave your email address below.",
     	                              widget=forms.TextInput(attrs={"class": "form-control-wide"}))
 
     feedback_question = forms.CharField(label="Your feedback",
@@ -12,3 +23,8 @@ class FeedbackForm(forms.Form):
     	                                widget=forms.Textarea(attrs={"rows": 5, 
     	                                                             "cols": 50, 
     	                                                             "class": "form-control-wide"}))
+
+    feedback_satisfaction = forms.ChoiceField(label="Overall, how satisfied were you with this service?",
+                                     choices=SATISFACTION_CHOICES,
+                                     required=False,
+                                     widget=RadioSelect(renderer=DSStackedRadioFieldRenderer))
