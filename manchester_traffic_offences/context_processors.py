cfddf79e11205a4ec5e69ded95eafa7d469f7947
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2015-03-04 09:46:41 +0000

    Add extra copy to emails
    Updated feedback copy at end of confirmation email, and adding
    base_url variable so links work on all environments
    
    [MAPDEV166]

--- manchester_traffic_offences/context_processors.py
+++ manchester_traffic_offences/context_processors.py
@@ -1,22 +1,33 @@
 from django.conf import settings
 
 def globals(request):
-  return {
-    # Application Title (Populates <title>)
-    'app_title': 'Make your plea: Traffic offences',
+    return {
+        # Application Title (Populates <title>)
+        'app_title': 'Make your plea: Traffic offences',
 
-    # Proposition Title (Populates proposition header)
-    'proposition_title': 'Make your plea: Traffic offences',
+        # Proposition Title (Populates proposition header)
+        'proposition_title': 'Make your plea: Traffic offences',
 
-    # Current Phase (Sets the current phase and the colour of phase tags). Presumed values: alpha, beta, live
-    'phase': 'alpha',
+        # Current Phase (Sets the current phase and the colour of phase tags). Presumed values: alpha, beta, live
+        'phase': 'alpha',
 
-    # Product Type (Adds class to body based on service type). Presumed values: information, service
-    'product_type': 'service',
+        # Product Type (Adds class to body based on service type). Presumed values: information, service
+        'product_type': 'service',
 
-    # Google Analytics ID (Tracking ID for the service)
-    'ga_id': 'UA-53811587-1',
+        # Google Analytics ID (Tracking ID for the service)
+        'ga_id': 'UA-53811587-1',
 
-    # Version number
-    'version': settings.VERSION
-  }
+        # Version number
+        'version': settings.VERSION
+        }
+
+def baseurl(request):
+    """
+    Return a base_url template context for the current request.
+    """
+    if request.is_secure():
+        scheme = 'https://'
+    else:
+        scheme = 'http://'
+
+    return {'base_url': scheme + request.get_host(),}
