c14f80ececfa9fb6c47de336d339230e2cda3ea5
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2015-03-04 15:50:38 +0000

    Add new/updated message and link to feedback screen
    [MAPDEV166]

--- apps/feedback/forms.py
+++ apps/feedback/forms.py
@@ -1,30 +1,31 @@
 from django import forms
 from django.forms.formsets import BaseFormSet
 from django.forms.widgets import RadioSelect
+from django.utils.translation import ugettext_lazy as _
 
 from apps.plea.fields import DSStackedRadioFieldRenderer
 
 class FeedbackForm(forms.Form):
     SATISFACTION_CHOICES = (
-        ("very satisfied", "very satisfied"),
-        ("satisfied", "satisfied"),
-        ("neither satisfied nor dissatisfied", "neither satisfied nor dissatisfied"),
-        ("dissatisfied", "dissatisfied"),
-        ("very dissatisfied", "very dissatisfied"))
+        ("very satisfied", _("very satisfied")),
+        ("satisfied", _("satisfied")),
+        ("neither satisfied nor dissatisfied", _("neither satisfied nor dissatisfied")),
+        ("dissatisfied", _("dissatisfied")),
+        ("very dissatisfied", _("very dissatisfied")))
 
-    feedback_email = forms.EmailField(label="Email address",
+    feedback_email = forms.EmailField(label=_("Email address"),
     	                              required=False,
-    	                              help_text="If you'd like us to get back to you, please leave your email address below.",
+    	                              help_text=_("If you'd like us to get back to you, please leave your email address below."),
     	                              widget=forms.TextInput(attrs={"class": "form-control-wide"}))
 
-    feedback_question = forms.CharField(label="Your feedback",
+    feedback_question = forms.CharField(label=_("Your feedback"),
     	                                required=True,
-    	                                error_messages={"required": "Please provide us with some feedback"},
+    	                                error_messages={"required": _("Please provide us with some feedback")},
     	                                widget=forms.Textarea(attrs={"rows": 5, 
     	                                                             "cols": 50, 
     	                                                             "class": "form-control-wide"}))
 
-    feedback_satisfaction = forms.ChoiceField(label="Overall, how satisfied were you with this service?",
+    feedback_satisfaction = forms.ChoiceField(label=_("Overall, how satisfied were you with this service?"),
                                      choices=SATISFACTION_CHOICES,
                                      required=False,
                                      widget=RadioSelect(renderer=DSStackedRadioFieldRenderer))
