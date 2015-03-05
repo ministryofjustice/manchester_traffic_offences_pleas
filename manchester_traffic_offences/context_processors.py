a16acf2c651608648400b242ccb3c01337a1d415
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2015-03-05 09:47:33 +0000

    Remove base_url
    This is not needed anywhere else than the emails.
    
    [MAPDEV166]

--- manchester_traffic_offences/context_processors.py
+++ manchester_traffic_offences/context_processors.py
@@ -20,14 +20,3 @@ def globals(request):
         # Version number
         'version': settings.VERSION
         }
-
-def baseurl(request):
-    """
-    Return a base_url template context for the current request.
-    """
-    if request.is_secure():
-        scheme = 'https://'
-    else:
-        scheme = 'http://'
-
-    return {'base_url': scheme + request.get_host(),}
