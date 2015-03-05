81336ded54e2e011f7508a076d118893f43024f6
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2015-03-05 10:27:02 +0000

    initial commit of user rating aggregate code

--- apps/feedback/tests.py
+++ apps/feedback/tests.py
@@ -1,3 +1,106 @@
-from django.test import TestCase
+import datetime as dt
 
-# Create your tests here.
+from django.core.urlresolvers import reverse
+from django.test import TestCase, RequestFactory
+
+from .models import UserRating, UserRatingAggregate
+from .views import feedback_form
+
+
+class FeedbackFormTestCase(TestCase):
+    def setUp(self):
+
+        self.request_factory = RequestFactory()
+
+        self.test_form_data = {
+            "user_satifaction": 1
+        }
+
+    def test_redirect_to_next_url(self):
+
+        request = self.request_factory.post(reverse("feedback_form"),
+                                            self.test_data)
+
+        response = feedback_form(request)
+
+        self.assertRedirects(response, reverse("home"))
+
+    def test_email_is_sent(self):
+
+        request = self.request_factory.post(reverse("feedback_form"),
+                                            self.test_data)
+
+        response = feedback_form(request)
+
+    def test_user_satisfaction_is_recorded(self):
+
+        assert not UserRating.objects.all()
+
+        request = self.request_factory.post(reverse("feedback_form"),
+                                            self.test_data)
+
+        feedback_form(request)
+
+        self.assertEquals(UserRating.objects.all().count(), 1)
+        self.assertEquals(
+            UserRating.objects.all()[0].user_satisfaction,
+            self.test_form_data["user_satifaction"])
+
+    def test_form_requires_validation(self):
+
+        request = self.request_factory.post(reverse("feedback_form"), {})
+
+        response = feedback_form(request)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, "feedback.html")
+
+
+def UserRatingTestCase(TestCase):
+
+    def test_weekly_aggregates(self):
+
+        UserRating.objects.record(5)
+
+        self.assertEquals(UserRating.objects.all().count(), 1)
+        self.assertEquals(UserRatingAggregate.objects.all().count(), 1)
+
+        rating = UserRating.objects.all()[0]
+        aggregate = UserRating.objects.all()[0]
+
+        self.assertEquals(rating.rating, 5)
+        self.assertEquals(aggregate.feedback_count, 1)
+        self.assertEquals(aggregate.feedback_total, 5)
+        self.assertEquals(aggregate.feedback_total, 5.0)
+
+        UserRating.objects.record(1)
+
+        self.assertEquals(UserRating.objects.all().count(), 2)
+        self.assertEquals(UserRatingAggregate.objects.all().count(), 1)
+
+        self.assertEquals(aggregate.feedback_count, 2)
+        self.assertEquals(aggregate.feedback_total, 6)
+        self.assertEquals(aggregate.feedback_avg, 3.0)
+
+    def test_submits_to_correct_week(self):
+
+        UserRating.objects.record(5)
+        UserRating.objects.record(1)
+
+        rating_obj = UserRating.objects.create(rating=3)
+        rating_obj.timestamp = dt.datetime.now() + dt.timedelta(7)
+        rating_obj.save()
+
+        UserRatingAggregate.objects.update_weekly_aggregate(rating_obj)
+
+        self.assertEquals(UserRatingAggregate.objects.all().count(), 2)
+
+        week1, week2 = UserRatingAggregate.objects.all()
+
+        self.assertEquals(week2.feedback_count, 2)
+        self.assertEquals(week2.feedback_total, 6)
+        self.assertEquals(week2.feedback_avg, 3.0)
+
+        self.assertEquals(week2.feedback_count, 1)
+        self.assertEquals(week2.feedback_total, 3)
+        self.assertEquals(week2.feedback_avg, 3.0)
