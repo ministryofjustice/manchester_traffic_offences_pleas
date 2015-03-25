1e73c5feeaa77ec0779ec77feda0ccc9e1cf1490
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2015-03-25 13:46:42 +0000

    Removed session.clear() from start view.
    The session.clear() interferred with the language selection and as the
    process is cleared at the end of the session, it should now be
    redundant.

--- manchester_traffic_offences/views.py
+++ manchester_traffic_offences/views.py
@@ -8,15 +8,12 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     """
-    Home page view, clears the session so that the user's session data
-    doesn't persist if they quit the process and start again. Otherwise
-    the form wizard automatically takes them to the success (or network
-    failure) page when they return to the form.
+    Home page view.
     """
     template_name = "start.html"
 
     def get(self, request, *args, **kwargs):
-        request.session.clear()
+
         return super(HomeView, self).get(request, *args, **kwargs)
 
 
