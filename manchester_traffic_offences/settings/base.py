b7cf4c7527f0bbab205ac502abdc62569f5d0e22
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2015-04-16 13:41:08 +0100

    re-enabled csrf protection and rate limiting

--- manchester_traffic_offences/settings/base.py
+++ manchester_traffic_offences/settings/base.py
@@ -108,7 +108,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.cache.UpdateCacheMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
