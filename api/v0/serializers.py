8ce1c8dc33a564fe9cab07e64c0602395c68dbb0
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2015-04-21 13:55:28 +0100

    initial commit still WIP

--- api/v0/serializers.py
+++ api/v0/serializers.py
@@ -1,17 +1,27 @@
 from rest_framework import serializers
 
-from apps.plea.models import Case, UsageStats
+from apps.plea.models import Case, UsageStats, Offence
+
+
+class OffenceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Offence
+        exclude = ("case",)
 
 
 class CaseSerializer(serializers.ModelSerializer):
-    urn = serializers.RegexField("^\d{2}/[a-zA-Z]{2}/\d+/\d{2}$", max_length=16, min_length=14)
+    urn = serializers.RegexField("^\d{2}/[a-zA-Z]{2}/\d+/\d{2}$", max_length=16, min_length=14)\
+
+    offences = OffenceSerializer(many=True)
 
     class Meta:
         model = Case
-        fields = ('urn',)
+        fields = ("urn", "title", "name", "forenames", "surname",
+                  "case_number")
 
     def validate(self, attrs):
-        if not Case.objects.can_use_urn(attrs['urn']):
+        if not Case.objects.can_use_urn(attrs["urn"]):
 
             raise serializers.ValidationError("Case data already exists")
 
