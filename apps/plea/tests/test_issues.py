64c4f6217a984805748bca2e017bbc21928edc86
Ian George <ian.george@digital.justice.gov.uk>
2015-04-22 15:21:32 +0100

    Hotfix for stage not set issue
    Users were somehow (still not identified how) getting back into the
    forms with a used URN still in session. The path for this wasn't
    quite dealing with the situation, this removes the code causing the
    error from the execution path, allowing the "URN already used" page
    to be displayed.
    
    [MAPDEV269]

---
+++ apps/plea/tests/test_issues.py
@@ -0,0 +1,40 @@
+import unittest
+
+from ..views import PleaOnlineForms
+from ..models import Case
+
+from test_plea_form import TestMultiPleaFormBase
+
+
+class TestPleaFormIssues(TestMultiPleaFormBase):
+    def setUp(self):
+        self.session = {}
+        self.request_context = {}
+
+        self.plea_stage_pre_data_1_charge = {"case": {"date_of_hearing": "2015-01-01",
+                                                      "urn_0": "06",
+                                                      "urn_1": "AA",
+                                                      "urn_2": "0000000",
+                                                      "urn_3": "00",
+                                                      "number_of_charges": 1,
+                                                      "company_plea": False},
+                                             "your_details": {"name": "Charlie Brown",
+                                                              "contact_number": "012345678",
+                                                              "email": "charliebrown@example.org"}}
+
+
+    def test_used_urn_in_session(self):
+        case = Case.objects.create(urn="06/AA/0000000/00", name="Ian George",
+                                   status="sent")
+        case.save()
+
+        self.session = {"case": {"complete": True,
+                                 "date_of_hearing": "2015-01-01",
+                                 "urn": "06/AA/0000000/00",
+                                 "number_of_charges": 1,
+                                 "company_plea": False}}
+
+        form = PleaOnlineForms("case", "plea_form_step", self.session)
+        form.save(self.plea_stage_pre_data_1_charge, self.request_context)
+
+        self.assertTrue(form._urn_invalid)
