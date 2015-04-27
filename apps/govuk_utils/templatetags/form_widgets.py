fc25ffc241df3c2c4118879b46484daccbb6ade5
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2015-04-27 15:16:31 +0100

    New template tags for all field types
    Tags added:
    {% wide_field ... %}
    {% radio_field ... %}
    {% money_field ... %}
    
    Templates updated to use new tags.
    
    [MAPDEV266]

--- apps/govuk_utils/templatetags/form_widgets.py
+++ apps/govuk_utils/templatetags/form_widgets.py
@@ -6,3 +6,18 @@ register = template.Library()
 def std_field(context, field, **kwargs):
     field.__dict__.update(kwargs)
     return {"field": field}
+
+@register.inclusion_tag('widgets/wide_field.html', takes_context=True)
+def wide_field(context, field, **kwargs):
+    field.__dict__.update(kwargs)
+    return {"field": field}
+
+@register.inclusion_tag('widgets/radio_field.html', takes_context=True)
+def radio_field(context, field, **kwargs):
+    field.__dict__.update(kwargs)
+    return {"field": field}
+
+@register.inclusion_tag('widgets/money_field.html', takes_context=True)
+def money_field(context, field, **kwargs):
+    field.__dict__.update(kwargs)
+    return {"field": field}
