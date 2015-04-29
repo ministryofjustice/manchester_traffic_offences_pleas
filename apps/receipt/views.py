f58009fceca53863d5d4b8f3184ba50c1ced5532
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2015-04-29 12:13:51 +0100

    Amends made to webhook to make it more secure
    Tests updated accordingly.
    
    [MAPDEV265-webhook]

--- apps/receipt/views.py
+++ apps/receipt/views.py
@@ -4,11 +4,13 @@ import sys
 import time
 import traceback
 
+from django.conf import settings
 from django.http import HttpResponse
 from django.views.generic import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
+from apps.plea.models import Court
 from .models import ReceiptLog
 from .process import process_receipt, InvalidFormatError, ReceiptProcessingError
 
@@ -29,10 +31,35 @@ class ReceiptWebhook(View):
     def dispatch(self, *args, **kwargs):
         return super(ReceiptWebhook, self).dispatch(*args, **kwargs)
 
-    def _get_stack_trace(self):
+    @staticmethod
+    def _get_stack_trace():
         ex_type, ex, tb = sys.exc_info()
         return "An exception has occured: {}\n\n{}"\
                .format(ex, traceback.format_tb(tb))
+
+    @staticmethod
+    def _validate_email(from_email, to_email, headers):
+        """
+        Validate the from/to email addresses and header
+
+        Returns True if the email appears to be valid, False if not,
+        and will create a ReceiptLog entry if the email is invalid.
+        """
+        if not Court.objects.validate_emails(from_email,
+                                             to_email):
+
+            return False, "to/from emails are not in the Court model {} {}".format(from_email, to_email)
+
+        header_check = getattr(settings, "RECEIPT_HEADER_FRAGMENT_CHECK", None)
+
+        if header_check:
+            for header in headers:
+                if header_check in header:
+                    break
+            else:
+                return False, "header fragment not found"
+
+        return True, ""
 
     def post(self, request, *args, **kwargs):
 
@@ -51,6 +78,17 @@ class ReceiptWebhook(View):
         status_text = []
 
         for item in items:
+
+            valid, reason = self._validate_email(item["msg"]["from_email"],
+                                                 item["msg"]["email"],
+                                                 item["msg"]["headers"])
+
+            if not valid:
+                status_text.append("Email not processed because: \n\n" + reason)
+
+                error_count += 1
+
+                continue
 
             try:
                 processed, status_text = process_receipt(item["msg"]["subject"],
