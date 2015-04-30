39ccb47e7d21d58e423bc9ae92b691c422feaa74
Ian George <ian.george@digital.justice.gov.uk>
2015-04-30 10:29:07 +0100

    Make the AD sites settings a bit easier to manage

--- manchester_traffic_offences/settings/staging_assisted.py
+++ manchester_traffic_offences/settings/staging_assisted.py
@@ -1,26 +1,5 @@
-from .base import *
-import os
+from .staging import *
 
-
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['POSTGRES_DB'],
-        'USER': os.environ['POSTGRES_USER'],
-        'PASSWORD': os.environ.get('POSTGRES_PASS', ''),
-        'HOST': os.environ.get('POSTGRES_HOST', ''),
-        'PORT': os.environ.get('POSTGRES_PORT', ''),
-    }
-}
-
-INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
 
 ALLOWED_HOSTS = ["assisted.makeaplea.dsd.io", ]
-SESSION_COOKIE_SECURE = True
-STORE_USER_DATA = True
-
-SITE_ID = 1
 EMAIL_REQUIRED = False
