884b0678da987d8f205fd0ec2cf7acbc5b2bf971
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2015-04-30 16:00:15 +0100

    Expenses fields not required
    The requirement for expenses fields was producing
    an overwhelming amount of errors if the user was
    trying to submit the form with empty fields,
    which is possible now that they are not pre-filled
    with 0.
    It is now up to the user to tell the service
    about their expenses: an empty field is treated
    as a 0.
    
    Note: the 'Optional' tags have been hidden to
    avoid cluttering the page design.
    
    [MAPDEV279]

--- apps/plea/stages.py
+++ apps/plea/stages.py
@@ -237,8 +237,8 @@ class YourExpensesStage(FormStage):
 
             self.set_next_step("review", skip=["company_finances"])
 
-            total_household = sum(clean_data[field] for field in household_expense_fields)
-            total_other = sum(clean_data[field] for field in other_expense_fields)
+            total_household = sum(int(clean_data[field] or 0) for field in household_expense_fields)
+            total_other = sum(int(clean_data[field] or 0) for field in other_expense_fields)
             total_expenses = total_household + total_other
 
             clean_data['total_household_expenses'] = total_household
