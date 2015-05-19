e6027fdae5a3e24075423214ad5b9771cc4b7960
Ian George <ian.george@digital.justice.gov.uk>
2015-05-19 17:07:36 +0100

    Fix production settings for new email routes

--- manchester_traffic_offences/settings/production.py
+++ manchester_traffic_offences/settings/production.py
@@ -5,8 +5,6 @@ import os
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -25,28 +23,13 @@ BROKER_TRANSPORT_OPTIONS = {'region': 'eu-west-1',
                             'queue_name_prefix': 'production-',
                             'polling_interval': 1}
 
+INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
+
 ALLOWED_HOSTS = ["www.makeaplea.justice.gov.uk", ]
 
 # Enable CachedStaticFilesStorage for cache-busting assets
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 
 SESSION_COOKIE_SECURE = True
-
-# Emails
-SMTP_ROUTES["GSI"]["HOST"] = os.environ.get('GSI_EMAIL_HOST', '')
-SMTP_ROUTES["GSI"]["PORT"] = int(os.environ.get('GSI_EMAIL_PORT', '25'))
-
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'email-smtp.eu-west-1.amazonaws.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USERNAME']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-EMAIL_USE_TLS = True
-
-PLEA_EMAIL_FROM = os.environ['PLEA_EMAIL_FROM']
-PLEA_EMAIL_TO = [os.environ['PLEA_EMAIL_TO'], ]
-PLP_EMAIL_TO = [os.environ["PLP_EMAIL_TO"], ]
-
-FEEDBACK_EMAIL_TO = [os.environ["FEEDBACK_EMAIL_TO"], ]
-FEEDBACK_EMAIL_FROM = os.environ["FEEDBACK_EMAIL_FROM"]
 
 STORE_USER_DATA = True
