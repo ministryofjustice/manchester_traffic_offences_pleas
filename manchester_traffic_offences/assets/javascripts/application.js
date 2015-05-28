dca0875916da902d1f08be587401e99caaf3fc61
Fred Marecesche <fred@froddd.co.uk>
2015-05-28 10:37:05 +0100

    Add External Links Tracking module
    New module allows sending a custom
    'External links' event to Google
    Analytics. Tests added.
    
    This update will need to be pushed
    to Staging to start seeing data in GA!
    
    [MAPDEV306]

--- manchester_traffic_offences/assets/javascripts/application.js
+++ manchester_traffic_offences/assets/javascripts/application.js
@@ -359,6 +359,77 @@ if(typeof String.prototype.trim !== 'function') {
   };
 
 }());
+(function() {
+  "use strict";
+
+  window.moj = window.moj || { Modules: {}, Events: $({}) };
+
+  var ExternalLinksTracker = function($el, options) {
+    this.init($el, options);
+    return this;
+  };
+
+  ExternalLinksTracker.prototype = {
+    defaults: {
+      "eventCategory": "External links",
+      "eventAction": document.location.pathname
+    },
+
+    init: function($el, options) {
+      if (typeof ga === "undefined") return;
+
+      this.settings = $.extend({}, this.defaults, options);
+      this.cacheElements($el);
+      this.bindEvents();
+    },
+
+    cacheElements: function($el) {
+      this.$links = $el;
+    },
+
+    bindEvents: function() {
+      var self = this;
+
+      this.$links.on('click', function(event) {
+        event.preventDefault();
+        self.sendGAEvent($(this));
+      });
+    },
+
+    sendGAEvent: function($el) {
+      var self = this,
+          href = $el.attr("href"),
+          target = $el.attr("target");
+
+      ga("send",
+         "event",
+         self.settings.eventCategory,
+         href,
+         self.settings.eventAction,
+         {"hitCallback": function() {
+            self.openLink(href, target);
+         }}
+      );
+    },
+
+    openLink: function(href, target) {
+      if (target && !/^_(self|parent|top)$/i.test(target)) {
+        window.open(href, target);
+      }
+      else {
+        window.location.href = href;
+      }
+    }
+  };
+
+  moj.Modules._ExternalLinksTracker = ExternalLinksTracker;
+
+  moj.Modules.ExternalLinksTracker = {
+    init: function() {
+      $('a[rel=external]').data('ExternalLinksTracker', new ExternalLinksTracker($(this), $(this).data()));
+    }
+  };
+}());
 /**
  * Page exit prompt
  *
@@ -577,5 +648,5 @@ $(document).ready(function () {
 
     var selectionButtons = new GOVUK.SelectionButtons($("label input[type='radio'], label input[type='checkbox']"));
 
-    $('details').details();
+    $("details").details();
 });
