5d59e8777cbbd54d2bb68eeb32162bb17de03a0c
Ian George <ian.george@digital.justice.gov.uk>
2015-06-10 14:00:04 +0000

    Added django-celery task backend to enable failed tasks to retry
    The retry on failed tasks was happening immediately instead of
    delaying 90 seconds as required. This was due to the tasks results
    being discarded. A django-orm backend has been added as a first
    pass with the option of moving it to redis or a cache later.
    
    [MAPDEV280]

--- manchester_traffic_offences/celery.py
+++ manchester_traffic_offences/celery.py
@@ -9,6 +9,5 @@ if not os.environ.has_key("DJANGO_SETTINGS_MODULE"):
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manchester_traffic_offences.settings.local')
 
 app = Celery('apps.plea')
-
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
