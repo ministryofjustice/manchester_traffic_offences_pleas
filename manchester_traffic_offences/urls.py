fbf32d749d510724b81923858083635e041c8734
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2015-07-01 16:24:08 +0100

    Added a /healthcheck url which does various diagnostic tests
    Note: some ops related changes required for this ticket have been
    made in an associated branch - MAPDEV314 (in ops)
    
    [MAPDEV320]

--- manchester_traffic_offences/urls.py
+++ manchester_traffic_offences/urls.py
@@ -26,5 +26,6 @@ urlpatterns = patterns("",
     url(r"^court-finder/$", CourtFinderView.as_view(), name="court_finder"),
     url(r"^change-language/$", views.set_language, name="set_language"),
     url(r"^session-timeout/$", TemplateView.as_view(template_name="session_timeout.html"), name="session_timeout"),
+    url(r"^", include("apps.monitoring.urls")),
 
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
