afe0031d03f44e0a826b2a3bfa8e3d8eafbd403e
Fred Marecesche <fred@froddd.co.uk>
2015-08-13 16:02:29 +0100

    Universal URN entry field
    Freeform entry field replaces split fields to allow entry
    of freeform URNs. The URN is automatically standardised and
    slashified, but users can omit slashes when entering the URN,
    to allow extra flexibility in respects to variations in notice
    formats between regions.
    
    Court finder updated to new field; validation for this has
    also been streamlined and uses correct validator: this also allows
    properly translating the error message, which wasn't yet done.
    
    Tests updated for all validators (including date ones).
    
    Also fixed JS issue: Google Analytics wasn't defined on
    Complete screen locally.
    
    [MAPDEV374]

---
+++ apps/plea/validators.py
@@ -0,0 +1,64 @@
+import datetime
+import re
+
+from django.core import exceptions
+from django.utils.translation import ugettext_lazy as _
+
+from .models import Case, Court
+from .standardisers import standardise_urn, slashify_urn
+
+
+def is_date_in_past(date):
+    if date >= datetime.datetime.today().date():
+        raise exceptions.ValidationError("The date must be in the past", code="is_date_in_past")
+
+    return True
+
+
+def is_date_in_future(date):
+    if date <= datetime.datetime.today().date():
+        raise exceptions.ValidationError("The date must be in the future", code="is_date_in_future")
+
+    return True
+
+
+def is_date_within_range(date):
+    if date > datetime.datetime.today().date()+datetime.timedelta(178):
+        raise exceptions.ValidationError("The date must be within the next 6 months", code="is_date_within_range")
+
+    return True
+
+
+def is_urn_valid(urn):
+    """
+    URN is 11 or 13 characters long in the following format:
+
+    00/AA/0000000/00
+    or
+    00/AA/00000/00
+    """
+
+    urn = slashify_urn(standardise_urn(urn))
+    pattern = r"[0-9]{2}/[a-zA-Z]{2}/(?:[0-9]{5}|[0-9]{7})/[0-9]{2}"
+
+    if not re.match(pattern, urn) or not Court.objects.has_court(urn):
+        raise exceptions.ValidationError("The URN is not valid", code="is_urn_valid")
+
+    court = Court.objects.get_by_urn(urn)
+    if court.validate_urn and not Case.objects.filter(urn__iexact=urn, sent=False).exists():
+        raise exceptions.ValidationError("The URN is not valid", code="is_urn_valid")
+
+    return True
+
+
+def is_urn_not_used(urn):
+    """
+    Check that the urn hasn't already been used in a previous submission
+    """
+
+    urn = slashify_urn(standardise_urn(urn))
+
+    if not Case.objects.can_use_urn(urn):
+        raise exceptions.ValidationError("The URN has already been used", code="is_urn_not_used")
+
+    return True
