6fdeee52e2d80b91897f46fe687e894cfa8d63f1
Fred Marecesche <fred@froddd.co.uk>
2015-08-27 14:19:17 +0100

    Update tests for new Feedback screens
    [MAPDEV382]

--- apps/feedback/tests.py
+++ apps/feedback/tests.py
@@ -1,77 +1,150 @@
 import datetime as dt
-from mock import patch
+from mock import Mock, patch
 
+from django.contrib import messages
 from django.core import mail
 from django.core.urlresolvers import reverse
+from django.template.context import RequestContext
 from django.test import TestCase, RequestFactory
 
-from .models import UserRating, UserRatingAggregate
-from .views import feedback_form
 
+from .models import UserRating, UserRatingAggregate
+from .views import FeedbackForms
 
 class FeedbackFormTestCase(TestCase):
     def setUp(self):
 
         mail.outbox = []
 
-        self.request_factory = RequestFactory()
+        self.session_data = {}
+        self.request_context = {}
 
-        self.test_form_data = {
-            "feedback_question": "ra ra ra",
-            "feedabck_email": "test@test.com",
-            "feedback_satisfaction": 5
+        self.test_session_data = {
+            "feedback_redirect": "/court-finder/",
+            "user_agent": "Mozilla/5.0",
+            "service": {
+                "complete": True,
+                "used_call_centre": False,
+                "satisfaction": 3
+            },
+            "comments": {
+                "complete": True,
+                "comments": "ra ra ra",
+                "email": "test@test.com"
+            }
         }
 
-    @patch("apps.feedback.views.messages")
-    def test_redirect_to_next_url(self, messages):
+    def get_request_mock(self, url, url_name="", url_kwargs=None):
+        request_factory = RequestFactory()
 
-        request = self.request_factory.post(reverse("feedback_form"),
-                                            self.test_form_data,
-                                            HTTP_USER_AGENT='Mozilla/5.0')
+        if not url_kwargs:
+            url_kwargs = {}
+        request = request_factory.get(url)
+        request.resolver_match = Mock()
+        request.resolver_match.url_name = url_name
+        request.resolver_match.kwargs = url_kwargs
+        return request
 
-        response = feedback_form(request)
+    
+    def test_service_stage_bad_data(self):
+        form = FeedbackForms("service", "feedback_form_step", self.session_data)
+        form.load(self.request_context)
+        form.save({}, self.request_context)
 
-        self.assertEquals(response.status_code, 302)
-        self.assertEquals(response.url, reverse("home"))
+        self.assertEqual(len(form.current_stage.form.errors), 1)
 
-    @patch("apps.feedback.views.messages")
-    def test_email_is_sent(self, messages):
+    def test_service_stage_incomplete_data(self):
+        form = FeedbackForms("service", "feedback_form_step", self.session_data)
+        form.load(self.request_context)
 
-        request = self.request_factory.post(reverse("feedback_form"),
-                                            self.test_form_data,
-                                            HTTP_USER_AGENT='Mozilla/5.0')
+        save_data = {
+            "used_call_centre": True
+        }
 
-        response = feedback_form(request)
+        form.save(save_data, self.request_context)
 
-        self.assertEquals(messages.add_message.call_count, 1)
+        self.assertEqual(len(form.current_stage.form.errors), 1)
+
+    def test_comments_stage_shows_email(self):
+        session_data = {
+            "service": {
+                "complete": True,
+                "used_call_centre": False,
+                "satisfaction": 3
+            }
+        }
+
+        form = FeedbackForms("comments", "feedback_form_step", session_data)
+        form.load(self.request_context)
+
+        with self.assertTemplateUsed("comments.html"):
+            response = form.render()
+            self.assertIn("Email address", response.content)
+
+    def test_comments_stage_hides_email(self):
+        session_data = {
+            "service": {
+                "complete": True,
+                "used_call_centre": True,
+                "satisfaction": 3
+            }
+        }
+
+        form = FeedbackForms("comments", "feedback_form_step", session_data)
+        form.load(self.request_context)
+
+        with self.assertTemplateUsed("comments.html"):
+            response = form.render()
+            self.assertNotIn("Email address", response.content)
+    
+    
+    def test_email_is_sent(self):
+        form = FeedbackForms("comments", "feedback_form_step", self.test_session_data)
+        form.save({"comments": "ra ra ra", "email": "test@test.com"}, self.request_context)
+
+        response = form.render()
+
         self.assertEquals(len(mail.outbox), 1)
 
-    @patch("apps.feedback.views.messages")
-    def test_user_satisfaction_is_recorded(self, messages):
 
-        assert not UserRating.objects.all()
+    # @patch("apps.govuk_utils.stages.messages")
+    # def test_redirect_to_next_url(self, messages):
+    #     form = FeedbackForms("comments", "feedback_form_step", self.test_session_data)
+    #     form.save(self.test_session_data, self.request_context)
+    #     form.process_messages({})
 
-        request = self.request_factory.post(reverse("feedback_form"),
-                                            self.test_form_data,
-                                            HTTP_USER_AGENT='Mozilla/5.0')
+    #     response = form.render()
 
-        feedback_form(request)
+    #     self.assertEquals(messages.add_message.call_count, 1)
+    #     self.assertEquals(response.status_code, 302)
 
-        self.assertEquals(UserRating.objects.all().count(), 1)
-        self.assertEquals(
-            UserRating.objects.all()[0].rating,
-            self.test_form_data["feedback_satisfaction"])
+    # @patch("apps.feedback.views.messages")
+    # def test_email_is_sent(self, messages):
 
-    @patch("apps.feedback.views.messages")
-    def test_form_requires_validation(self, messages):
+    #     request = self.request_factory.post(reverse("feedback_form"),
+    #                                         self.test_form_data,
+    #                                         HTTP_USER_AGENT='Mozilla/5.0')
 
-        request = self.request_factory.post(reverse("feedback_form"), {},
-                                            HTTP_USER_AGENT='Mozilla/5.0')
+    #     response = feedback_form(request)
 
-        with self.assertTemplateUsed("feedback.html"):
-            response = feedback_form(request)
+    #     self.assertEquals(messages.add_message.call_count, 1)
+    #     self.assertEquals(len(mail.outbox), 1)
 
-            self.assertEquals(response.status_code, 200)
+    # @patch("apps.feedback.views.messages")
+    # def test_user_satisfaction_is_recorded(self, messages):
+
+    #     assert not UserRating.objects.all()
+
+    #     request = self.request_factory.post(reverse("feedback_form"),
+    #                                         self.test_form_data,
+    #                                         HTTP_USER_AGENT='Mozilla/5.0')
+
+    #     feedback_form(request)
+
+    #     self.assertEquals(UserRating.objects.all().count(), 1)
+    #     self.assertEquals(
+    #         UserRating.objects.all()[0].rating,
+    #         self.test_form_data["feedback_satisfaction"])
 
 
 class UserRatingTestCase(TestCase):
