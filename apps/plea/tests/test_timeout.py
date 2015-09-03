8f31391dfd83c50a36961f970174c254ce809e83
Fred Marecesche <fred@froddd.co.uk>
2015-09-03 16:20:27 +0100

    Update Plea journey to use its own session key
    This prevents the whole session having to be nuked on
    completion, meaning things like language settings get
    preserved: the feedback now appears in Welsh after
    completing the journey in Welsh.
    
    [MAPDEV406]

--- apps/plea/tests/test_timeout.py
+++ apps/plea/tests/test_timeout.py
@@ -10,7 +10,7 @@ class TestTimeout(TestCase):
     def setUp(self):
         self.client = Client()
         # http://code.djangoproject.com/ticket/10899
-        settings.SESSION_ENGINE = 'django.contrib.sessions.backends.file'
+        settings.SESSION_ENGINE = "django.contrib.sessions.backends.file"
         engine = import_module(settings.SESSION_ENGINE)
         store = engine.SessionStore()
         store.save()
@@ -18,9 +18,9 @@ class TestTimeout(TestCase):
         self.client.cookies[settings.SESSION_COOKIE_NAME] = store.session_key
 
     def test_no_urn_no_refresh_headers(self):
-        response = self.client.get('/plea/case/')
+        response = self.client.get("/plea/case/")
 
-        self.assertEqual(response.has_header('Refresh'), False)
+        self.assertEqual(response.has_header("Refresh"), False)
 
     def test_no_session_in_request(self):
         request = {}
@@ -32,12 +32,13 @@ class TestTimeout(TestCase):
 
     def test_when_urn_has_refresh_headers(self):
         session = self.session
-        session["case"] = {"urn": "51/AA/00000/00"}
+        session["plea"] = {}
+        session["plea"]["case"] = {"urn": "51/AA/00000/00"}
         session.save()
 
-        response = self.client.get('/plea/case/')
+        response = self.client.get("/plea/case/")
 
         wait = str(getattr(settings, "SESSION_COOKIE_AGE", 3600));
 
-        self.assertEqual(response.has_header('Refresh'), True)
+        self.assertEqual(response.has_header("Refresh"), True)
         self.assertTrue("Refresh: " + wait + "; url=/session-timeout/" in response.serialize_headers())
