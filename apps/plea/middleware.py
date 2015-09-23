40626ab8a4bfcdb6fe86969ec6a626a2993430dc
Fred Marecesche <fred@froddd.co.uk>
2015-09-23 14:43:16 +0100

    Change middleware session key check
    When updating the Plea data session key, this hadn't been
    updated, causing the refresh headers to never be set. Tests
    were had the old session key hardcoded, and were therefore
    passing.
    
    [MAPDEV432]

--- apps/plea/middleware.py
+++ apps/plea/middleware.py
@@ -4,9 +4,9 @@ class TimeoutRedirectMiddleware:
 
     def process_response(self, request, response):
         try:
-            if (request.session.get("plea", {}).get("case", {}).get("urn", False)):
+            if (request.session.get("plea_data", {}).get("case", {}).get("urn", False)):
                 response["Refresh"] = str(getattr(settings, "SESSION_COOKIE_AGE", 3600)) + "; url=/session-timeout/"
         except AttributeError:
             pass
-        
+
         return response
