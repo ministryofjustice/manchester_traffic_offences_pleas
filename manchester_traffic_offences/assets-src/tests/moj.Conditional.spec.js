94320b0478dd12626f32c444c27bff204de3a254
Fred Marecesche <fred@froddd.co.uk>
2015-09-23 10:28:41 +0100

    Update Conditional module
    No need for aria-hidden="false" attribute, as this is ignored
    by all browsers and assistive readers.
    
    Added aria-controls attribute to inputs.
    
    Tests updated to run faster: set up and tear down are only fired once.
    
    Remove need for class, simplify main data attribute
    
    Add aria-live="polite" attribute to conditionals:
    
    In most screen readers, this should ensure the contents are read
    at an appropriate time when the contents are revealed.
    
    Also added HTML5 'hidden' attribute for better assistive tech support.
    
    [MAPDEV434]

--- manchester_traffic_offences/assets-src/tests/moj.Conditional.spec.js
+++ manchester_traffic_offences/assets-src/tests/moj.Conditional.spec.js
@@ -1,22 +1,25 @@
 describe("moj.Conditional", function() {
-	var $fixture, subject;
+	var $fixture = $(
+    '<div class="test_control">' +
+      '<input id="radio_1" type="radio" name="trigger_name" value="regex">' +
+      '<input id="radio_2" type="radio" name="trigger_name" value="another value" aria-controls="test">' +
+      '<input id="radio_3" type="radio" name="trigger_name" value="yet another value" aria-controls="target_1">' +
+      '<div id="target_1" data-conditional="trigger_name" data-conditional-value="^regex$">...</div>' +
+    '</div>'
+  );
+  var subject;
 
-	beforeEach(function() {
-		$fixture = $(
-      '<div class="test_control">'
-      + '<input id="radio_1" type="radio" name="trigger_name" value="regex">'
-      + '<input id="radio_2" type="radio" name="trigger_name" value="another value">'
-      + '<div class="js-Conditional" id="target_1" data-conditional-trigger="trigger_name" data-conditional-value="^regex$">...</div>'
-      + '</div>'
-		);
+  beforeAll(function() {
     $fixture.appendTo('body');
-    subject = new moj.Modules._Conditional($('.js-Conditional'));
-	});
-
-  afterEach(function() {
-    $('body').find('.test_control').remove();
+    subject = new moj.Modules._Conditional($('[data-conditional]'));
+    // Remove moj JS console output for testing
+    moj.Modules.devs = {};
+    moj.Events.trigger('render');
   });
 
+  afterAll(function() {
+    $('body').find('.test_control').remove();
+  });
 
   it("should be an object", function() {
     expect(typeof subject).toBe('object');
@@ -24,17 +27,7 @@ describe("moj.Conditional", function() {
 
   it("should cache elements", function() {
     expect(subject.$conditional.length).toBe(1);
-    expect(subject.$inputs.length).toBe(2);
-  });
-
-  it("should add ARIA attributes to the inputs", function() {
-    expect(subject.$inputs.attr('aria-controls')).toBeDefined();
-  });
-
-  it("should call toggle() when the input's change event is fired", function() {
-    spyOn(subject, 'toggle');
-    subject.$inputs.trigger('change');
-    expect(subject.toggle).toHaveBeenCalled();
+    expect(subject.$inputs.length).toBe(3);
   });
 
   it("should call toggle() when moj.Events' render even is fired", function() {
@@ -43,23 +36,44 @@ describe("moj.Conditional", function() {
     expect(subject.toggle).toHaveBeenCalled();
   });
 
-  it("should not show the conditional if no input is checked", function() {
-    moj.Events.trigger('render');
-    expect($('.js-Conditional')).toBeHidden();
+  it("should add ARIA attributes", function() {
+    expect($('#radio_1').attr('aria-controls')).toBe('target_1');
+    expect($('#target_1').attr('aria-expanded')).toBe('false');
+    expect($('#target_1').attr('aria-live')).toBe('polite');
   });
-  
+
+  it("should not remove existing ARIA attributes if they are already set", function() {
+    expect($('#radio_2').attr('aria-controls')).toBe('test target_1');
+  });
+
+  it("should only add ARIA attributes if they don't exist", function() {
+    expect($('#radio_3').attr('aria-controls')).toBe('target_1');
+  });
+
+  it("should not show the conditional if no input is checked", function() {
+    expect($('#target_1')).toBeHidden();
+  });
+
+  it("should call toggle() when the input's change event is fired", function() {
+    spyOn(subject, 'toggle');
+    subject.$inputs.trigger('change');
+    expect(subject.toggle).toHaveBeenCalled();
+  });
+
   it("should show the conditional if the value matches", function() {
     $('#radio_1').trigger('click');
-    expect($('.js-Conditional')).toBeVisible();
-    expect($('.js-Conditional').attr('aria-expanded')).toBe('true');
-    expect($('.js-Conditional').attr('aria-hidden')).toBe('false');
+    expect($('#target_1')).toBeVisible();
+    expect($('#target_1').attr('aria-expanded')).toBe('true');
+    expect($('#target_1').attr('aria-hidden')).toBeUndefined();
+    expect($('#target_1').prop('hidden')).toBe(false);
   });
 
   it("should hide the conditional if the value doesn't match", function() {
     $('#radio_2').trigger('click');
-    expect($('.js-Conditional')).toBeHidden();
-    expect($('.js-Conditional').attr('aria-expanded')).toBe('false');
-    expect($('.js-Conditional').attr('aria-hidden')).toBe('true');
+    expect($('#target_1')).toBeHidden();
+    expect($('#target_1').attr('aria-expanded')).toBe('false');
+    expect($('#target_1').attr('aria-hidden')).toBe('true');
+    expect($('#target_1').prop('hidden')).toBe(true);
   });
 
 });
