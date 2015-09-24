6a3894239f23786663425806c84a97bade16547d
Fred Marecesche <fred@froddd.co.uk>
2015-09-24 17:52:44 +0100

    Proper focusing on alert elements and inputs
    Added role="alert" to message headers (success, error summaries).
    The focus is automatically given to first of these elements.
    The script that handles the 'skip link' has been moved.
    Clicking on error messages focuses the relevant field.
    
    [MAPDEV440]

--- manchester_traffic_offences/assets-src/javascripts/application.js
+++ manchester_traffic_offences/assets-src/javascripts/application.js
@@ -1,11 +1,4 @@
-$(document).ready(function () {
-    jQuery.fx.off = true;
-
-    $('.nojs-only[name=split_form]').remove();
-
-    $('.skiplink').on('click', function() {
-      $('#content').attr('tabindex', -1).on('blur focusout', function() {
-        $(this).removeAttr('tabindex');
-      }).focus();
-    });
+$(function() {
+  jQuery.fx.off = true;
+  $('.nojs-only[name=split_form]').remove();
 });
