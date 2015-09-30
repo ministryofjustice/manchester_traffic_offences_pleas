a1a14ba3a46c8d8dc33dafcf4f90fc1356b61d39
Fred Marecesche <fred@froddd.co.uk>
2015-09-30 13:46:22 +0100

    Move session timeout check to middleware
    Moved all session timeout processing to middleware, so it is accessible
    by all views. This now checks for session data for both Plea and
    Feedback journeys, as the feedback is now split into 2 screens.
    
    Updated markup to include sessionTimeout JS variable used by
    PromptOnChange module. The alternative message variable is also now a JS
    var.
    
    [MAPDEV446]

--- apps/govuk_utils/middleware.py
+++ apps/govuk_utils/middleware.py
@@ -1,5 +1,32 @@
+import time
+
 from django.conf import settings
 from django.utils import translation
+
+def get_session_timeout(request):
+    try:
+        if (request.session.get("plea_data", {}).get("case", False)
+            or request.session.get("feedback_data", {}).get("service", False)):
+            timeout = int(time.time() + getattr(settings, "SESSION_COOKIE_AGE", 3600))
+            return timeout
+    except AttributeError:
+        pass
+
+    return False
+
+class TimeoutRedirectMiddleware:
+
+    def process_request(self, request):
+        session_timeout = get_session_timeout(request);
+        if session_timeout:
+            request.session_timeout = session_timeout
+
+    def process_response(self, request, response):
+
+        if hasattr(request, "session_timeout"):
+            response["Refresh"] = str(getattr(settings, "SESSION_COOKIE_AGE", 3600)) + "; url=/session-timeout/"
+
+        return response
 
 
 class AdminLocaleURLMiddleware:
