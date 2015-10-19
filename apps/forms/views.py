9f7b00a58ff90782685c008182ba51c379c2aca2
Ian George <ian.george@digital.justice.gov.uk>
2015-10-19 12:01:11 +0000

    Fixed a session timeout issue introduced by indexed plea pages, refactored apps.plea.views.PleaOnlineViews

--- apps/forms/views.py
+++ apps/forms/views.py
@@ -1,12 +1,12 @@
 from django.views.generic import TemplateView
 
-class StorageView(TemplateView):
 
-    def _get_storage(self, request, session_key):
+class StorageView(TemplateView):
+    def get_storage(self, request, session_key):
         if not request.session.get(session_key):
             request.session[session_key] = {}
 
         return request.session[session_key]
 
-    def _clear_storage(self, request, session_key):
+    def clear_storage(self, request, session_key):
         del request.session[session_key]
