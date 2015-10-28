d62ef26b9179a3ac49ea20195504fa063078844a
Ian George <ian.george@digital.justice.gov.uk>
2015-10-28 17:26:38 +0000

    Add per-region URN validators and normalisers

--- apps/plea/tests/test_standardisers.py
+++ apps/plea/tests/test_standardisers.py
@@ -2,13 +2,15 @@ from django.test import TestCase
 
 from ..standardisers import *
 
+
 class TestStandardisers(TestCase):
 
     def test_blank_standardised_urn(self):
+        standardise = get_standardiser("")
         with self.assertRaises(StandardiserNoOutputException):
-            standardise_urn("")
+            standardise("")
 
-    def test_non_blank_standardised_urn(self):
+    def test_standardise_urn(self):
         urns = {"00/AA/11111/99": "00AA1111199",
                 "00/aa/11111/99": "00AA1111199",
                 "0-0-aa_11111-99": "00AA1111199",
@@ -19,10 +21,22 @@ class TestStandardisers(TestCase):
         for urn, output in urns.items():
             self.assertEquals(output, standardise_urn(urn))
 
-
-    def test_slashify_urn(self):
+    def test_format_urn(self):
         urns = {"00AA1111199": "00/AA/11111/99",
                 "00AA222222299": "00/AA/2222222/99"}
 
         for urn, output in urns.items():
-            self.assertEquals(output, slashify_urn(urn))
+            self.assertEquals(output, format_urn(urn))
+
+    def test_standardise_met_urn(self):
+        urns = {"02TJDS0479/15/0014AP": "02TJDS0479150014AP",
+                "02TjDs0479/15/0014aP": "02TJDS0479150014AP"}
+
+        for urn, output in urns.items():
+            self.assertEquals(output, standardise_urn(urn))
+
+    def test_format_met_urn(self):
+        urns = {"02TJDS0479150014AP": "02TJDS0479/15/0014AP"}
+
+        for urn, output in urns.items():
+            self.assertEquals(output, format_met_urn(urn))
