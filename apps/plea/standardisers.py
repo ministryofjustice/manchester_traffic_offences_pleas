d943aa98b2ed4c00a97ac38e9cab9d47b7369adf
Ian George <ian.george@digital.justice.gov.uk>
2015-10-28 17:26:38 +0000

    Add per-region URN validators and normalisers

--- apps/plea/standardisers.py
+++ apps/plea/standardisers.py
@@ -1,7 +1,21 @@
 import re
 
+
 class StandardiserNoOutputException(Exception):
     pass
+
+
+def get_standardiser(urn):
+    return URN_STANDARDISERS.get(urn[:2], URN_STANDARDISERS["*"])
+
+
+def normalise_standard_urn(urn):
+    return format_urn(standardise_urn(urn))
+
+
+def normalise_met_urn(urn):
+    return format_met_urn(standardise_urn(urn))
+
 
 def standardise_urn(urn):
     """
@@ -16,8 +30,18 @@ def standardise_urn(urn):
     return output
 
 
-def slashify_urn(urn):
+def format_urn(urn):
     """
     Inserts slashes in a standardised URN
     """
     return urn[:2] + "/" + urn[2:4] + "/" + urn[4:-2] + "/" + urn[-2:]
+
+
+def format_met_urn(urn):
+    return urn[:10] + "/" + urn[10:12] + "/" + urn[12:]
+
+
+URN_STANDARDISERS = {
+    "02": normalise_met_urn,
+    "*": normalise_standard_urn
+}
