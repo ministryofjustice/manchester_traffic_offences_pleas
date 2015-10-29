0c2e920aa3b80961889c61f9a1768215e76af231
Ian George <ian.george@digital.justice.gov.uk>
2015-10-29 14:11:50 +0000

    Update the standardisers, validators and tests to cope with the new and interesting reference numbers we're seeing in the wild

--- apps/plea/tests/test_standardisers.py
+++ apps/plea/tests/test_standardisers.py
@@ -30,13 +30,22 @@ class TestStandardisers(TestCase):
 
     def test_standardise_met_urn(self):
         urns = {"02TJDS0479/15/0014AP": "02TJDS0479150014AP",
-                "02TjDs0479/15/0014aP": "02TJDS0479150014AP"}
+                "02TjDs0479/15/0014aP": "02TJDS0479150014AP",
+                "02TJ/AA0000/00/0015aa": "02TJAA0000000015AA",
+                "02TJ/AA0000/00/00151aa": "02TJAA00000000151AA",
+                "02TJ/AA0000/00/00151aaa": "02TJAA00000000151AAA",
+                "02TJC00000000aa": "02TJC00000000AA",
+                "02TJC00000000aaa": "02TJC00000000AAA",
+                "02TJ0000000000000000aa": "02TJ0000000000000000AA",
+                "02TJ0000000000000000aaa": "02TJ0000000000000000AAA",
+                "02TJ0000000/00aa": "02TJ000000000AA",
+                "02TJ0000000/00aaa": "02TJ000000000AAA"}
 
         for urn, output in urns.items():
             self.assertEquals(output, standardise_urn(urn))
 
     def test_format_met_urn(self):
-        urns = {"02TJDS0479150014AP": "02TJDS0479/15/0014AP"}
+        urns = {"02TJDS0479150014AP": "02TJDS0479150014AP"}
 
         for urn, output in urns.items():
             self.assertEquals(output, format_met_urn(urn))
