19ccf208d548d3175aeebfe01ef572b824d5d45a
Ian George <ian.george@digital.justice.gov.uk>
2015-11-09 17:49:57 +0000

    Added SJP area field, updated court admin, service now checks if area is SJP ready before allowing the choice of notice type

---
+++ apps/plea/migrations/0011_court_sjp_area.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('plea', '0010_auto_20151029_0926'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='court',
+            name='sjp_area',
+            field=models.BooleanField(default=False, help_text=b'Is this area sending out SJP notices?'),
+        ),
+    ]
