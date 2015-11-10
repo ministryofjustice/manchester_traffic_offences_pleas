550ac534a93f122a8a09a127d0ab6054f7ea74ac
Ian George <ian.george@digital.justice.gov.uk>
2015-11-10 15:39:03 +0000

    Tidied up some of the tests for style, now 2 are broken so @froddd is going to take a look

--- apps/plea/tests/test_models.py
+++ apps/plea/tests/test_models.py
@@ -6,65 +6,62 @@ from django.test import TestCase
 from ..models import CourtEmailCount, UsageStats, Court
 
 
-def seed_stats(self):
-    """
-    Populates the test DB with test data: 3 Court objects,
-    and several CourtEmailCount objects.
-    """
-    self.court_1 = Court.objects.create(
-        court_name="Court 01",
-        region_code="01",
-        test_mode=False,
-        enabled=True, court_address="x", court_telephone="x", court_email="x", submission_email="x")
-
-    self.court_2 = Court.objects.create(
-        court_name="Court 02",
-        region_code="02",
-        test_mode=False,
-        enabled=True, court_address="x", court_telephone="x", court_email="x", submission_email="x")
-
-    self.court_3 = Court.objects.create(
-        court_name="Court 03, Test mode",
-        region_code="03",
-        test_mode=True,
-        enabled=True, court_address="x", court_telephone="x", court_email="x", submission_email="x")
-
-
-    """
-    Create dummy CourtEmailCount entries. Switching off auto_now_add
-    allows the save date to be faked.
-    """
-
-    for field in CourtEmailCount._meta.local_fields:
-        if field.name == "date_sent":
-            field.auto_now_add = False
-
-    # Court 1, week starting 05/01/2015
-    CourtEmailCount.objects.create(court=self.court_1, total_pleas=1, total_guilty=1, total_not_guilty=0, date_sent=dt.date(2015, 1, 5), hearing_date=dt.date(2015, 1, 29), sent=True) # 24 days
-    CourtEmailCount.objects.create(court=self.court_1, total_pleas=1, total_guilty=0, total_not_guilty=1, date_sent=dt.date(2015, 1, 5), hearing_date=dt.date(2015, 1, 29), sent=True) # 24 days
-    CourtEmailCount.objects.create(court=self.court_1, total_pleas=2, total_guilty=2, total_not_guilty=0, date_sent=dt.date(2015, 1, 5), hearing_date=dt.date(2015, 1, 29), sent=True) # 24 days
-    CourtEmailCount.objects.create(court=self.court_1, total_pleas=3, total_guilty=2, total_not_guilty=1, date_sent=dt.date(2015, 1, 9), hearing_date=dt.date(2015, 1, 30), sent=True) # 21 days
-
-    # Court 1, week starting 12/01/2015
-    CourtEmailCount.objects.create(court=self.court_1, total_pleas=2, total_guilty=1, total_not_guilty=1, date_sent=dt.date(2015, 1, 12), hearing_date=dt.date(2015, 1, 30), sent=True) # 18 days
-    CourtEmailCount.objects.create(court=self.court_1, total_pleas=2, total_guilty=1, total_not_guilty=1, date_sent=dt.date(2015, 1, 12), hearing_date=dt.date(2015, 1, 30), sent=True) # 18 days
-    CourtEmailCount.objects.create(court=self.court_1, total_pleas=2, total_guilty=1, total_not_guilty=1, date_sent=dt.date(2015, 1, 14), hearing_date=dt.date(2015, 1, 31), sent=True) # 17 days
-
-    # Court 2, week starting 12/01/2015
-    CourtEmailCount.objects.create(court=self.court_2, total_pleas=3, total_guilty=3, total_not_guilty=0, date_sent=dt.date(2015, 1, 12), hearing_date=dt.date(2015, 1, 30), sent=True) # 18 days
-
-    # Test court, week starting 12/01/2015
-    CourtEmailCount.objects.create(court=self.court_3, total_pleas=3, total_guilty=0, total_not_guilty=3, date_sent=dt.date(2015, 1, 12), hearing_date=dt.date(2015, 1, 30), sent=True)
-
-    for field in CourtEmailCount._meta.local_fields:
-        if field.name == "date_sent":
-            field.auto_now_add = True
-
-
-class CourtEmailCountManagerTestCase(TestCase):
+class TestStatsBase(TestCase):
     def setUp(self):
-        seed_stats(self)
+        """
+        Populates the test DB with test data: 3 Court objects,
+        and several CourtEmailCount objects.
+        """
+        self.court_1 = Court.objects.create(
+            court_name="Court 01",
+            region_code="01",
+            test_mode=False,
+            enabled=True, court_address="x", court_telephone="x", court_email="x", submission_email="x")
 
+        self.court_2 = Court.objects.create(
+            court_name="Court 02",
+            region_code="02",
+            test_mode=False,
+            enabled=True, court_address="x", court_telephone="x", court_email="x", submission_email="x")
+
+        self.court_3 = Court.objects.create(
+            court_name="Court 03, Test mode",
+            region_code="03",
+            test_mode=True,
+            enabled=True, court_address="x", court_telephone="x", court_email="x", submission_email="x")
+
+        """
+        Create dummy CourtEmailCount entries. Switching off auto_now_add
+        allows the save date to be faked.
+        """
+
+        for field in CourtEmailCount._meta.local_fields:
+            if field.name == "date_sent":
+                field.auto_now_add = False
+
+        # Court 1, week starting 05/01/2015
+        CourtEmailCount.objects.create(court=self.court_1, total_pleas=1, total_guilty=1, total_not_guilty=0, date_sent=dt.date(2015, 1, 5), hearing_date=dt.date(2015, 1, 29), sent=True) # 24 days
+        CourtEmailCount.objects.create(court=self.court_1, total_pleas=1, total_guilty=0, total_not_guilty=1, date_sent=dt.date(2015, 1, 5), hearing_date=dt.date(2015, 1, 29), sent=True) # 24 days
+        CourtEmailCount.objects.create(court=self.court_1, total_pleas=2, total_guilty=2, total_not_guilty=0, date_sent=dt.date(2015, 1, 5), hearing_date=dt.date(2015, 1, 29), sent=True) # 24 days
+        CourtEmailCount.objects.create(court=self.court_1, total_pleas=3, total_guilty=2, total_not_guilty=1, date_sent=dt.date(2015, 1, 9), hearing_date=dt.date(2015, 1, 30), sent=True) # 21 days
+
+        # Court 1, week starting 12/01/2015
+        CourtEmailCount.objects.create(court=self.court_1, total_pleas=2, total_guilty=1, total_not_guilty=1, date_sent=dt.date(2015, 1, 12), hearing_date=dt.date(2015, 1, 30), sent=True) # 18 days
+        CourtEmailCount.objects.create(court=self.court_1, total_pleas=2, total_guilty=1, total_not_guilty=1, date_sent=dt.date(2015, 1, 12), hearing_date=dt.date(2015, 1, 30), sent=True) # 18 days
+        CourtEmailCount.objects.create(court=self.court_1, total_pleas=2, total_guilty=1, total_not_guilty=1, date_sent=dt.date(2015, 1, 14), hearing_date=dt.date(2015, 1, 31), sent=True) # 17 days
+
+        # Court 2, week starting 12/01/2015
+        CourtEmailCount.objects.create(court=self.court_2, total_pleas=3, total_guilty=3, total_not_guilty=0, date_sent=dt.date(2015, 1, 12), hearing_date=dt.date(2015, 1, 30), sent=True) # 18 days
+
+        # Test court, week starting 12/01/2015
+        CourtEmailCount.objects.create(court=self.court_3, total_pleas=3, total_guilty=0, total_not_guilty=3, date_sent=dt.date(2015, 1, 12), hearing_date=dt.date(2015, 1, 30), sent=True)
+
+        for field in CourtEmailCount._meta.local_fields:
+            if field.name == "date_sent":
+                field.auto_now_add = True
+
+
+class CourtEmailCountManagerTestCase(TestStatsBase):
     def test_calculate_aggregates(self):
 
         totals = CourtEmailCount.objects.calculate_aggregates(dt.date(2015, 1, 5), 7)
@@ -84,10 +81,7 @@ class CourtEmailCountManagerTestCase(TestCase):
         self.assertEquals(totals['not_guilty'], 0)
 
 
-class CourtEmailCountModelTestCase(TestCase):
-    def setUp(self):
-        seed_stats(self)
-
+class CourtEmailCountModelTestCase(TestStatsBase):
     def test_get_from_context_hearing_date_is_combined_date_and_time(self):
         context = {
             "notice_type": {"sjp": False},
@@ -104,7 +98,6 @@ class CourtEmailCountModelTestCase(TestCase):
         emailcount.get_from_context(context, self.court_1)
 
         self.assertEqual(emailcount.hearing_date, dt.datetime(2014, 9, 22))
-
 
     def test_court_email_plea__get_from_context__sc_char_count(self):
 
@@ -140,7 +133,6 @@ class CourtEmailCountModelTestCase(TestCase):
         self.assertEqual(email_count.sc_guilty_char_count, 13)
         self.assertEqual(email_count.sc_not_guilty_char_count, 16)
 
-
     def test_get_stats(self):
 
         totals = CourtEmailCount.objects.get_stats()
@@ -150,19 +142,16 @@ class CourtEmailCountModelTestCase(TestCase):
         self.assertEquals(totals["guilty"], 11)
         self.assertEquals(totals["not_guilty"], 5)
 
-
-    def get_stats_start_date(self):
+    def test_get_stats_start_date(self):
 
         totals = CourtEmailCount.objects.get_stats(start="2015-01-12")
 
-        print totals
         self.assertEquals(totals["submissions"], 7)
         self.assertEquals(totals["pleas"], 9)
         self.assertEquals(totals["guilty"], 6)
         self.assertEquals(totals["not_guilty"], 3)
 
-
-    def get_stats_end_date(self):
+    def test_get_stats_end_date(self):
 
         totals = CourtEmailCount.objects.get_stats(end="2015-01-12")
 
@@ -171,13 +160,11 @@ class CourtEmailCountModelTestCase(TestCase):
         self.assertEquals(totals["guilty"], 5)
         self.assertEquals(totals["not_guilty"], 2)
 
-
     def test_get_stats_by_hearing_date(self):
 
         stats = CourtEmailCount.objects.get_stats_by_hearing_date()
 
         self.assertEqual(len(stats), 3)
-
 
     def test_get_stats_by_hearing_date_1_day(self):
 
@@ -190,7 +177,6 @@ class CourtEmailCountModelTestCase(TestCase):
         self.assertEqual(stats[0]["guilty"], 3)
         self.assertEqual(stats[0]["not_guilty"], 1)
 
-
     def test_get_stats_by_hearing_date_1_day_date_specified(self):
 
         stats = CourtEmailCount.objects.get_stats_by_hearing_date(days=1, start_date="2015-01-30")
@@ -202,7 +188,6 @@ class CourtEmailCountModelTestCase(TestCase):
         self.assertEqual(stats[0]["guilty"], 7)
         self.assertEqual(stats[0]["not_guilty"], 3)
 
-
     def test_get_stats_by_court(self):
 
         stats = CourtEmailCount.objects.get_stats_by_court()
@@ -213,7 +198,6 @@ class CourtEmailCountModelTestCase(TestCase):
         self.assertEqual(stats[0]["pleas"], 13)
         self.assertEqual(stats[0]["guilty"], 8)
         self.assertEqual(stats[0]["not_guilty"], 5)
-
 
     def test_get_stats_days_from_hearing(self):
 
@@ -230,11 +214,10 @@ class CourtEmailCountModelTestCase(TestCase):
 
         self.assertEqual(len(stats), 5)
 
-class UsageStatsTestCase(TestCase):
 
+class UsageStatsTestCase(TestStatsBase):
     def setUp(self):
-        seed_stats(self)
-
+        super(UsageStatsTestCase, self).setUp()
         # wednesday
         self.to_date = dt.date(2015, 01, 21)
 
