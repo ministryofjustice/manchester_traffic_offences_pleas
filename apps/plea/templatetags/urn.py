07bf0e8cce82612a6c73d33568586ba047f2c2b7
Ian George <ian.george@digital.justice.gov.uk>
2015-11-23 14:15:05 +0000

    Updated the API to return validation errors (400) on blank, invalid, or duplicate URNs on case creation.

--- apps/plea/templatetags/urn.py
+++ apps/plea/templatetags/urn.py
@@ -10,7 +10,8 @@ register = template.Library()
 @register.filter
 def format_urn(obj):
     """
-    HTML version of dir.
+    Re-format a standardised URN with any slashes or extra characters
+    re-inserted.
     """
     formatted_urn = format_for_region(obj)
     return mark_safe(formatted_urn)
