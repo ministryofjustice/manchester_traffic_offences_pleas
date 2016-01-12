202a98d29793e574630c2229a515577487a7a240
Ian George <ian.george@digital.justice.gov.uk>
2016-01-12 14:32:34 +0000

    Add migration for CaseOffenceFilter model

---
+++ apps/plea/migrations/0026_caseoffencefilter.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('plea', '0025_auto_20151210_1526'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='CaseOffenceFilter',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('filter_match', models.CharField(max_length=20)),
+                ('description', models.CharField(max_length=500, null=True, blank=True)),
+            ],
+        ),
+    ]
