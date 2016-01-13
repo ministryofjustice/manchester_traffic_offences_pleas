a8e9a670c020e736d9a398305622162674168a06
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2016-01-13 16:50:32 +0000

    Adding a docker settings file for the api

---
+++ api/settings/docker.py
@@ -0,0 +1,55 @@
+from .base import *
+import os
+
+
+DEBUG = os.environ.get("DJANGO_DEBUG", "") == "True"
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB', ''),
+        'USER': os.environ.get('DB_USERNAME', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', ''),
+    }
+}
+
+PREMAILER_OPTIONS = {"base_url": "http://dev.makeaplea.dsd.io",
+                     "remove_classes": False,
+                     "keep_style_tags": True,
+                     "cssutils_logging_level": logging.ERROR}
+
+SMTP_ROUTES["GSI"]["USERNAME"] = os.environ.get("GSI_EMAIL_USERNAME", "")
+SMTP_ROUTES["GSI"]["PASSWORD"] = os.environ.get("GSI_EMAIL_PASSWORD", "")
+SMTP_ROUTES["PNN"]["USERNAME"] = os.environ.get("PNN_EMAIL_USERNAME", "")
+SMTP_ROUTES["PNN"]["PASSWORD"] = os.environ.get("PNN_EMAIL_PASSWORD", "")
+
+BROKER_TRANSPORT_OPTIONS = {'region': 'eu-west-1',
+                            'queue_name_prefix': os.environ.get("CELERY_QUEUE_POLLING_PREFIX", "dev-"),
+                            'polling_interval': 1,
+                            'visibility_timeout': 3600}
+
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", "localhost:8000"), ]
+
+# Enable CachedStaticFilesStorage for cache-busting assets
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
+
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+
+CELERY_ALWAYS_EAGER = os.environ.get("CELERY_ALWAYS_EAGER", False)
+BROKER_URL = os.environ.get("CELERY_BROKER_URL", "SQS://")
+
+#
+# Temporary keys to run collectstatic on docker image build.
+#
+# Override in your environment.
+#
+SECRET_KEY = os.environ.get("SECRET_KEY", "***REMOVED***")
+ENCRYPTED_COOKIE_KEYS = [
+    os.environ.get("ENCRYPTED_COOKIE_KEY", "***REMOVED***")
+]
+
+STORE_USER_DATA = os.environ.get("STORE_USER_DATA", "") == "True"
