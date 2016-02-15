1a54477b2da2bfd81550f598d21a20d8ca345711
Ian George <ian.george@digital.justice.gov.uk>
2016-02-15 11:39:12 +0000

    Storing the company name for the wrong condition

--- apps/plea/email.py
+++ apps/plea/email.py
@@ -87,7 +87,7 @@ def send_plea_email(context_data):
     case.language = translation.get_language().split("-")[0]
     case.name = standardise_name(first_name, last_name)
 
-    if context_data["case"]["plea_made_by"] == "Defendant":
+    if context_data["case"]["plea_made_by"] == "Company representative":
         if case.extra_data and "OrganisationName" in case.extra_data:
             case.extra_data["OrganisationName"] = context_data.get("company_details", {}).get("company_name")
         else:
@@ -96,6 +96,7 @@ def send_plea_email(context_data):
     if send_user_email and email_address:
         case.email = email_address
         case.send_user_email = True
+
     case.save()
 
     if getattr(settings, "STORE_USER_DATA", False):
