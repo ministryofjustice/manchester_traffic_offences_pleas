efa617cbe4bbe10777a7bf007bd0aae9ffb20bbb
Ian George <ian.george@digital.justice.gov.uk>
2016-02-15 11:40:07 +0000

    Updated the URN entry replaying routine to update the existing data set rather than importing a CSV, this is the more common use case recently

--- make_a_plea/management/commands/replay_urn_entries.py
+++ make_a_plea/management/commands/replay_urn_entries.py
@@ -1,30 +1,16 @@
-import csv
 from django.core.management.base import BaseCommand
 
-
 from apps.plea.models import DataValidation, Case
-from apps.plea.standardisers import standardise_urn, format_for_region
 
 
 class Command(BaseCommand):
-    help = "Build weekly aggregate stats"
-
-    def add_arguments(self, parser):
-        parser.add_argument('csv_file', nargs='+')
+    help = "Re-runs the urn data validation entries to find matches in the current data set"
 
     def handle(self, *args, **options):
-        print options["csv_file"]
-        with open(options['csv_file'][0]) as csvfile:
-            reader = csv.reader(csvfile)
-            for idx, row in enumerate(reader):
-                urn = row[2]
-                dv = DataValidation()
-                dv.urn_entered = urn
-                dv.urn_standardised = standardise_urn(urn)
-                dv.urn_formatted = format_for_region(dv.urn_standardised)
-                cases = Case.objects.filter(urn=dv.urn_standardised)
-                dv.case_match_count = len(cases)
-                if len(cases) > 0:
-                    dv.case_match = cases[0]
+        for dv in DataValidation.objects.all():
+            cases = Case.objects.filter(urn=dv.urn_standardised)
+            dv.case_match_count = len(cases)
+            if len(cases) > 0:
+                dv.case_match = cases[0]
 
-                dv.save()
+            dv.save()
