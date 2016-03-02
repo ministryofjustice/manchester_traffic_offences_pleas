1eeee9c760980b854e5d84a67f580c55030a990c
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2016-03-02 16:28:09 +0000

    Add sentry for staging and production environments
    Re-introduced settings files for staging and prod rather than trying to
    configure everything in a single docker.py settings file.
    
    Removed unused settings files.
    
    The settings files can be set via template-deploy using
    DJANGO_SETTINGS_MODULE

--- make_a_plea/settings/production.py
+++ make_a_plea/settings/production.py
@@ -1,45 +1,10 @@
-from .base import *
-import os
+from .docker import *
 
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
+INSTALLED_APPS.append('raven.contrib.django.raven_compat')
 
-GOOGLE_ANALYTICS_ID = "UA-53811587-1"
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['POSTGRES_DB'],
-        'USER': os.environ['POSTGRES_USER'],
-        'PASSWORD': os.environ.get('POSTGRES_PASS', ''),
-        'HOST': os.environ.get('POSTGRES_HOST', ''),
-        'PORT': os.environ.get('POSTGRES_PORT', ''),
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
-    }
+RAVEN_CONFIG = {
+    'dsn': os.environ["SENTRY_DSN"],
+    'release': os.environ.get("APP_GIT_COMMIT", "no-git-commit-available")
 }
 
-BROKER_TRANSPORT_OPTIONS = {'region': 'eu-west-1',
-                            'queue_name_prefix': 'production-',
-                            'polling_interval': 1,
-                            'visibility_timeout': 3600}
-
-ALLOWED_HOSTS = ["www.makeaplea.justice.gov.uk", "www.makeaplea.service.gov.uk"]
-
-# Enable CachedStaticFilesStorage for cache-busting assets
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
-
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-
-STORE_USER_DATA = True
-
-ENCRYPTED_COOKIE_KEYS = [
-    os.environ["ENCRYPTED_COOKIE_KEY"]
-]
-
-REDIRECT_START_PAGE = "https://www.gov.uk/make-a-plea"
-
-RATE_LIMIT = "5000/m"
