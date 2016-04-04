fe9c5e1e67ba8639aaa17fc7c6e2a185207336b2
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2016-04-04 18:56:38 +0100

    Refactoring result code adding tests and migrating logic into models

--- apps/result/models.py
+++ apps/result/models.py
@@ -1,4 +1,45 @@
+from decimal import Decimal
+import re
+
 from django.db import models
+
+from apps.plea.models import Case, CaseOffenceFilter
+
+
+DO_NOT_RESULT_CODES = {
+    "DDDT": "Discretionary with disqualification until ordinary test passed",
+    "DDPR": "Points but reduced disqualification for mitigating circumstance",
+    "DDOTE": "Obligatory disqualification until extended test passed",
+    "DDOR": "Obligatory but reduced disqualification for special reasons",
+    "DDP": "Points disqualification",
+    "DDO": "Obligatory disqualification",
+    "DDD": "Discretionary with ordinary disqualification",
+    "DDPTE": "Points with disqualification until extended test passed",
+    "DDRN": "Disqualification for non-endorsable offence",
+    "DDRCOT": "Obligatory Disqualification until extended test passed - reduction for course",
+    "DDRI": "Interim Disqualification",
+    "DDDTO": "Discretionary disqualification until test passed only",
+    "DDRCD": "Driving Disq - Reduction for course (discretionary)",
+    "DDRCO": "Driving Disq - Reduction for course (obligatory)"
+}
+
+
+ADJOURNED_CODES = {
+    "AREMHA": "Adjournment Adult Remittal Home Court Area",
+    "AREMOA": "Adjournment Adult Remittal Other Court Area",
+    "A": "Adjournment",
+    "AINT": "Adjourn Interim Hearing",
+    "AO": "Adjournment - Other Court Area",
+    "ADJP": "Adjournment (Document requires personal service)",
+    "AREMHY": "Adjournment Youth Remittal Home Court Area",
+    "ACON": "Account Consolidated",
+    "ADJNN": "Adjournment (No Notice)",
+    "ASD": "Adjourned Sine Die"
+}
+
+WITHDRAWN_CODES = {
+    "WDRN": "Withdrawn"
+}
 
 
 class Result(models.Model):
@@ -19,6 +60,92 @@ class Result(models.Model):
     processed = models.BooleanField(default=False)
     sent = models.BooleanField(default=False)
     sent_on = models.DateTimeField(null=True, blank=True)
+
+    def has_valid_offences(self):
+        """
+        Are all the offences in this case whistlisted?
+        """
+
+        offence_codes = [offence.offence_code[:4] for offence in self.result_offences.all()]
+
+        for offence_code in offence_codes:
+            if CaseOffenceFilter.objects.filter(filter_match__startswith=offence_code).exists():
+                return True
+
+        return False
+
+    def can_result(self):
+        """
+        Can a case be resulted?
+
+        All offence results need to be fit the criteria below:
+
+        1. All offence codes in scope / whitelisted? Yes, then we can result
+        1. Disqualification? No result
+        2. Does it have final codes? Yes, then we can result
+        3. An offence has been adjourned? Yes, then we can't result
+        4. An offence has been adjourned but subsequently withdrawn? If yes, then we can result
+        """
+
+        has_final_codes = False
+
+        for result in self.result_offences.all():
+            adjourned = False
+            withdrawn = False
+
+            for offence_data in result.offence_data.all():
+                if offence_data.result_code[0].upper() == "F":
+                    has_final_codes = True
+
+                if offence_data.result_code in DO_NOT_RESULT_CODES:
+                    return False, "out of scope result code: {}".format(offence_data.result_code)
+
+                if offence_data.result_code in ADJOURNED_CODES:
+                    adjourned = True
+
+                if offence_data.result_code in WITHDRAWN_CODES:
+                    withdrawn = True
+
+            if adjourned and not withdrawn:
+                return False, "adjournment"
+
+        if not has_final_codes:
+            return False, "no final codes"
+        else:
+            return True, ""
+
+    def get_associated_case(self):
+        """
+        Return an associated and resultable case
+        """
+
+        cases = Case.objects.filter(case_number=self.case_number, email__isnull=False, sent=True)
+
+        return cases[0] if cases else None
+
+    def get_offence_totals(self):
+        """
+        Extract relevant offence information
+        """
+
+        total = Decimal()
+        fines = []
+        endorsements = []
+
+        for offence in self.result_offences.all():
+            for r in offence.offence_data.all():
+                if r.result_code.startswith("F"):
+                    values = re.findall(r'\xa3([0-9]+\.*[0-9]{0,2})', r.result_wording)
+                    value = sum(Decimal(v) for v in values)
+                    total += value
+                    fines.append(r.result_wording)
+
+                elif r.result_code in ["LEP", ]:
+                    endorsements.append(r.result_wording)
+                # else:
+                #     print offence.offence_code, r.result_code, r.result_wording.encode("utf-8")
+
+        return fines, endorsements, total
 
 
 class ResultOffence(models.Model):
