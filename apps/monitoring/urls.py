a121e6f129ea241928cc440a29946be2d4186e05
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2016-10-14 10:13:42 +0100

    Require admin login to view service status page

--- apps/monitoring/urls.py
+++ apps/monitoring/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
-from django.views.decorators.cache import cache_page
 
 from .views import CourtDataView
 
 
 urlpatterns = (
-    url(r"service-status/", cache_page(60*60*24)(CourtDataView.as_view())),
+    url(r"service-status/", CourtDataView.as_view()),
 )
