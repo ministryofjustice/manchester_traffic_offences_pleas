ee8c5be33b959069e33fba65cbdeb1f37ee40b5d
Alexa <alexaballantine@Alexas-MacBook-Pro.local>
2017-04-22 15:29:24 +0100

    tests running but need some fixes

--- cucumber/features/step_definitions/landing_page_steps.rb
+++ cucumber/features/step_definitions/landing_page_steps.rb
@@ -7,10 +7,17 @@ When(/^I click on the start now button$/) do
 end
 
 Then(/^I should see page header$/) do
-  expect(landing_page.content).to have_page_header
+  expect(landing_page.content.page_header.text).to eq 'Make a plea for a traffic offence'
 end
 
-Then(/^I should be taken to your case page$/) do
+Then(/^I should see get started intro$/) do
+  expect(landing_page.content).to have_get_started_intro
+end
+
+Then(/^I should be taken to "Your case" page$/) do
   expect(your_case_page.content.page_header.text).to eq 'Your case'
 end
 
+And(/^I should see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
