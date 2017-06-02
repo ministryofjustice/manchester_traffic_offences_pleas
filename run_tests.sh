c59589b0e07c85b3832a30e9e8f1483cb08a3a34
Richard Clark <richard.clark@digital.justice.gov.uk>
2017-06-02 11:31:27 +0000

    Fixed: Applied feedback from PR

--- run_tests.sh
+++ run_tests.sh
@@ -25,7 +25,7 @@ fi
 DJANGO_SETTINGS_MODULE=make_a_plea.settings.testing
 export DJANGO_SETTINGS_MODULE
 
-pip install -Ur requirements/testing.txt 2>&1
+pip install -r requirements/testing.txt 2>&1
 python ./manage.py compilemessages 
 
 coverage erase;
