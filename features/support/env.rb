789c23ed38d4fe4bc048d0d490b6fb82d0987098
Alexa <alexa.ballantine@gmail.com>
2017-07-20 15:47:14 +0100

    RST-214 removed env and env* from gitignore

---
+++ features/support/env.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require 'site_prism'
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/cucumber'
+require 'capybara/poltergeist'
+require 'capybara-screenshot/cucumber'
+
+require File.dirname(__FILE__) + '/../support/capybara_driver_helper'
+require File.dirname(__FILE__) + '/../support/site_prism_config'
