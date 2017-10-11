506c37d3438f2707a35bb29f213c44e01f669dca
Leonidas Tsampros <ltsampros@gmail.com>
2017-10-11 16:32:09 +0100

    Change personal emails for Admins to HMCTS Reform email groups (#391)

--- api/settings/production.py
+++ api/settings/production.py
@@ -3,6 +3,10 @@ from .docker import *
 
 INSTALLED_APPS.append('raven.contrib.django.raven_compat')
 
+ADMINS = (
+    ('HMCTS Reform Sustaining Support', 'sustainingteamsupport@HMCTS.NET'),
+)
+
 RAVEN_CONFIG = {
     'dsn': os.environ["SENTRY_DSN"],
     'release': os.environ.get("APP_GIT_COMMIT", "no-git-commit-available")
