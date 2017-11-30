f7dc244114b9411091a5c27395f13e552a54e5fa
Karol Grecki <kgrecki@users.noreply.github.com>
2017-11-30 13:29:19 +0000

    Update local dev set up and readme
    * Add docker file for dev/qa
    * Update readme

--- vagrant/reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Reset the database and dependencies to those on master
-
-git checkout master
-sudo -u postgres dropdb manchester_traffic_offences && sudo -u postgres createdb manchester_traffic_offences
-
-./vagrant/update.sh
