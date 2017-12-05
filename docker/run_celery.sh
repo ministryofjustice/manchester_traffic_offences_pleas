9f1d8de25a5ce7e2952fc7b3d8abf28f8d60db38
Karol Grecki <karol.grecki@solirius.com>
2017-12-05 15:35:10 +0000

    Restore docker directory and gpg file

---
+++ docker/run_celery.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export PATH=$PATH:/makeaplea/
+
+export C_FORCE_ROOT=true
+
+cd /makeaplea && source /makeaplea/docker/celery_defaults && celery worker -A make_a_plea.celery:app --loglevel INFO
+
