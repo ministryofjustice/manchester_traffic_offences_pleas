4fe950c8a3b86bc57ee5ca8aa24da149257a103e
Max Awan <max.awan@digital.justice.gov.uk>
2018-06-14 15:14:43 +0100

    Changed data retention to 20 weeks

--- make_a_plea/settings/base.py
+++ make_a_plea/settings/base.py
@@ -339,7 +339,7 @@ FTP_SERVER_IP = os.environ.get("FTP_SERVER_IP", "")
 
 AXES_COOLOFF_TIME = 1
 
-DATA_RETENTION_PERIOD = int(os.environ.get("DATA_RETENTION_PERIOD", "84"))
+DATA_RETENTION_PERIOD = int(os.environ.get("DATA_RETENTION_PERIOD", "140"))
 
 RAVEN_CONFIG = {
     'dsn': os.environ.get("SENTRY_DSN", ""),
