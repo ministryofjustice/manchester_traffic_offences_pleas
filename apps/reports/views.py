939b440e4250ec76bb7dcf8400cb6d95ab17c9bd
Max Awan <max.awan@digital.justice.gov.uk>
2018-12-11 06:30:26 +0000

    feedback report, date picker and time filtering for all reports

--- apps/reports/views.py
+++ apps/reports/views.py
@@ -7,12 +7,24 @@ from django.conf import settings
 from django.shortcuts import render
 from django.views import View
 from django.urls import reverse, reverse_lazy
+from django.core.urlresolvers import resolve
+from urllib import quote, urlencode
 from ..plea.models import UsageStats, CaseTracker
-from .charts import RequiredStagesChart, FinancialSituationChart, HardshipChart, AllStagesDropoutsChart, IncomeSourcesDropoutsChart
+from ..feedback.models import UserRating
+from .charts import RequiredStagesChart, FinancialSituationChart,\
+    HardshipChart, AllStagesDropoutsChart, IncomeSourcesDropoutsChart
 from django.db.models import Value, Sum
 import datetime
-
+from .charts import safe_percentage
 # Create your views here.
+
+
+def build_url(*args, **kwargs):
+    get = kwargs.pop('get', {})
+    url = reverse(*args, **kwargs)
+    if get:
+        url += '?' + urlencode(get)
+    return url
 
 @login_required(login_url=settings.ADMIN_LOGIN_URL)
 def index(request):
@@ -54,6 +66,14 @@ class StageMixin(object):
         self.report_description = "A statistical analysis of stage progression"
 
 
+class UserRatingMixin(object):
+
+    def initialize_get(self, request):
+        self.report_name = "User rating report"
+        self.report_url = reverse("reports:rating_report")
+        self.report_description = "An analysis of service user ratings"
+
+
 class PleaReportEntry(PleaMixin, ReportEntryView):
     pass
 
@@ -62,18 +82,49 @@ class StageReportEntry(StageMixin, ReportEntryView):
     pass
 
 
+class RatingReportEntry(UserRatingMixin, ReportEntryView):
+    pass
+
+
 class BaseReportView(ReportEntryView):
     report_partial = None
     template = "report_base.html"
+    start_date = None
+    end_date = None
+
+    def update_context_with_period(self, request, context):
+        day_start = quote(datetime.date.today().strftime('%d/%m/%Y'))
+        week_start = datetime.date.today() - datetime.timedelta(days=7)
+        week_start = quote(week_start.strftime('%d/%m/%Y'))
+        month_start = quote(subtract_one_month(datetime.date.today()).strftime('%d/%m/%Y'))
+        today = quote(datetime.date.today().strftime('%d/%m/%Y'))
+        context["day_url"] = build_url("reports:" + resolve(request.path).url_name, get={'start_date':day_start, 'end_date':today})
+        context["week_url"] = build_url("reports:" + resolve(request.path).url_name, get={'start_date':week_start, 'end_date':today})
+        context["month_url"] = build_url("reports:" + resolve(request.path).url_name, get={'start_date':month_start, 'end_date':today})
+        return context
 
     def get_context_data(self, request):
         current_context_data = super(BaseReportView, self).get_context_data(request)
         current_context_data["report_partial"] = self.report_partial
         current_context_data.update(self.prepare_report_context(request))
+        current_context_data = self.update_context_with_period(request, current_context_data)
         return current_context_data
 
     def prepare_report_context(self, request):
         pass
+
+    def set_start_end_dates(self, request):
+        if 'start_date' in request.GET:
+            try:
+                self.start_date = datetime.datetime.strptime(request.GET['start_date'], '%d/%m/%Y').date()
+            except ValueError:
+                self.start_date = None
+
+        if 'end_date' in request.GET:
+            try:
+                self.end_date = datetime.datetime.strptime(request.GET['end_date'], '%d/%m/%Y').date()
+            except ValueError:
+                self.end_date = None
 
 
 class PleaReportView(PleaMixin, BaseReportView):
@@ -82,29 +133,19 @@ class PleaReportView(PleaMixin, BaseReportView):
 
     def prepare_report_context(self, request):
         # Ensure correct start and end dates for report
-        if 'start_date' in request.GET:
-            try:
-                start_date = datetime.datetime.strptime(request.GET['start_date'], '%d/%m/%Y').date()
-            except ValueError:
-                start_date = None
-
-        if 'end_date' in request.GET:
-            try:
-                end_date = datetime.datetime.strptime(request.GET['end_date'], '%d/%m/%Y').date()
-            except ValueError:
-                end_date = None
+        self.set_start_end_dates(request)
         change_date = datetime.date(day=21,month=5,year=2018)
         qs = UsageStats.objects.all()
         late_end_date = True  # Set to true if end date is after 21st May
         early_start_date = True
-        if start_date:
-            qs = qs.filter(start_date__gte=start_date)
-            start_date = start_date.date()
+        if self.start_date:
+            qs = qs.filter(start_date__gte=self.start_date)
+            start_date = self.start_date
             if start_date > change_date:
                 early_start_date = False
-        if end_date:
-            qs = qs.filter(start_date__lt=end_date)
-            end_date = end_date.date()
+        if self.end_date:
+            qs = qs.filter(start_date__lt=self.end_date)
+            end_date = self.end_date
             if end_date < change_date:
                 late_end_date = False
         pre_qs = qs.filter(start_date__lt=change_date)
@@ -135,8 +176,8 @@ class PleaReportView(PleaMixin, BaseReportView):
         post_online_guilty_no_court_pleas = post_totals["online_guilty_no_court_pleas__sum"] or 0
         post_online_pleas = post_online_not_guilty_pleas + post_online_guilty_pleas
         return {
-            'start_date': start_date,
-            'end_date': end_date,
+            'start_date': self.start_date,
+            'end_date': self.end_date,
             'late_end_date': late_end_date,
             'early_start_date': early_start_date,
             'all_online_submissions' : all_online_submissions,
@@ -170,31 +211,18 @@ class StageReportView(StageMixin, BaseReportView):
                     (reverse_lazy("reports:income_sources_dropouts_report"), "Income sources dropouts",
                      "The probability of dropping out after"
                      " selecting (self)employed/receiving out-of-work benefits/other"))
-    start_date = None
-    end_date = None
-    chart_name = "the required stages"
+    chart_name = "Required stages"
 
     def call_correct_chart(self):
         return RequiredStagesChart(self.start_date, self.end_date)
 
     def prepare_report_context(self, request):
         # Ensure correct start and end dates for report
-        if 'start_date' in request.GET:
-            try:
-                start_date = datetime.datetime.strptime(request.GET['start_date'], '%d/%m/%Y').date()
-            except ValueError:
-                start_date = None
-
-        if 'end_date' in request.GET:
-            try:
-                end_date = datetime.datetime.strptime(request.GET['end_date'], '%d/%m/%Y').date()
-            except ValueError:
-                end_date = None
-
+        self.set_start_end_dates(request)
         bar_chart = self.call_correct_chart()
         return {
-            'start_date': start_date,
-            'end_date': end_date,
+            'start_date': self.start_date,
+            'end_date': self.end_date,
             'case_count': bar_chart.get_case_count(),
             'complete_count': bar_chart.get_complete_count(),
             'completion_percentage': bar_chart.get_completion_percentage(),
@@ -236,3 +264,49 @@ class IncomeSourcesDropoutsView(StageReportView):
 
     def call_correct_chart(self):
         return IncomeSourcesDropoutsChart(self.start_date, self.end_date)
+
+
+class RatingReportView(UserRatingMixin, BaseReportView):
+    report_partial = "partials/rating_report_contents.html"
+    chart_name = "User ratings"
+
+    def prepare_report_context(self, request):
+        self.set_start_end_dates(request)
+        qs = UserRating.objects.all()
+        if self.start_date:
+            qs = qs.filter(timestamp__gte=self.start_date)
+        if self.end_date:
+            qs = qs.filter(timestamp__lte=self.end_date)
+        tot_count = qs.count()
+        vdis_count = qs.filter(service_rating=1).count()
+        dis_count = qs.filter(service_rating=2).count()
+        neither_count = qs.filter(service_rating=3).count()
+        sat_count = qs.filter(service_rating=4).count()
+        vsat_count = qs.filter(service_rating=5).count()
+        bar_chart = [["Very dissatisfied".encode('ascii','ignore'), vdis_count],
+                    ["Dissatisfied".encode('ascii','ignore'), dis_count],
+                    ["Neither satisfied nor dissatisfied".encode('ascii','ignore'), neither_count],
+                    ["Satisfied".encode('ascii','ignore'), sat_count],
+                    ["Very satisfied".encode('ascii','ignore'), vsat_count]]
+        rating_number_array = [vdis_count, dis_count, neither_count, sat_count, vsat_count]
+        rating_percentage_array = [safe_percentage(vdis_count, tot_count), safe_percentage(dis_count, tot_count),
+                                   safe_percentage(neither_count, tot_count), safe_percentage(sat_count, tot_count),
+                                   safe_percentage(vsat_count, tot_count)]
+        return {
+            'start_date': self.start_date,
+            'end_date': self.end_date,
+            'bar_chart': bar_chart,
+            "no_of_responses": tot_count,
+            'no_of_each_response': rating_number_array,
+            'percentage_of_each_response': rating_percentage_array,
+            'chart_name': self.chart_name,
+        }
+
+
+def subtract_one_month(dt0):
+    current_date = dt0.day
+    dt1 = dt0.replace(day=1)
+    dt2 = dt1 - datetime.timedelta(days=1)
+    dt3 = dt2.replace(day=min(current_date, 28))
+
+    return dt3
