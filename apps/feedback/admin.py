56b49f11ac1b64b5dadcbcdd6235d2494221cad6
Max Awan <max.awan@digital.justice.gov.uk>
2019-01-29 12:22:38 +0000

    enable filtering by rating properties and link to admin from rating report

--- apps/feedback/admin.py
+++ apps/feedback/admin.py
@@ -2,7 +2,7 @@ import csv
 
 from django.contrib import admin
 from django.http import HttpResponse
-
+from django.core.validators import EMPTY_VALUES
 from .models import UserRating, UserRatingAggregate
 
 
@@ -52,12 +52,36 @@ def performance_platform_export(modeladmin, request, queryset):
 performance_platform_export.short_description = "Export for performance platform"
 
 
+class CommentsFilter(admin.SimpleListFilter):
+    """Allow filtering Cases by Comment including compound views"""
+
+    title = u'comments'
+    parameter_name = u'comments'
+
+    def lookups(self, request, model_admin):
+        return(
+            ('0', 'Commented', ),
+            ('1', 'Not commented',),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() in EMPTY_VALUES:
+            return queryset
+        exclude = (self.value() == '0')
+        kwargs = {'{0}__exact'.format(self.parameter_name): ''}
+        method = queryset.exclude if exclude else queryset.filter
+        return method(**kwargs)
+
+
 class UserRatingAdmin(admin.ModelAdmin):
+
     list_display = ("timestamp", "service_rating", "call_centre_rating", "comments")
+    list_filter = ("timestamp", "service_rating", "call_centre_rating", CommentsFilter)
 
 
 class UserRatingAggregateAdmin(admin.ModelAdmin):
-    list_display = ("start_date", "question_tag", "question_text", "rating_1", "rating_2", "rating_3", "rating_4", "rating_5", "total")
+    list_display = ("start_date", "question_tag", "question_text", "rating_1", "rating_2", "rating_3", "rating_4",
+                    "rating_5", "total")
 
     actions = [performance_platform_export]
 
