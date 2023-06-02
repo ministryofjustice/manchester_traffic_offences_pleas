3800a23bd19f17d839ac2685ce9a143ae7ef7ea3
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-06-02 12:13:20 +0100

    change STATIC_ROOT from static to staticfiles

--- make_a_plea/settings/base.py
+++ make_a_plea/settings/base.py
@@ -91,7 +91,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = root('static')
+STATIC_ROOT = root('staticfiles')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
