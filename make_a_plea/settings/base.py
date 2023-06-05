9b6e83e8afc8160969f514aa29e3ac6a11af9cda
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-06-05 12:20:42 +0100

    Revert "change value of STATICFILES_DIRS"
    This reverts commit be38e55f70bedfa83eba11c8e5d2df7a41a8396a.

--- make_a_plea/settings/base.py
+++ make_a_plea/settings/base.py
@@ -99,7 +99,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    root('static'),
+    root('assets'),
 )
 
 # List of finder classes that know how to find static files in
