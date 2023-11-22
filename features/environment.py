84e4c3c592d97b55ae26a8944adfad6d4e776cab
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-11-22 14:44:08 +0000

    update location to chrome

--- features/environment.py
+++ features/environment.py
@@ -81,7 +81,7 @@ def before_all(context):
             # chrome_options.binary_location = "/usr/local/bin/chrome"
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
-            chrome_driver_binary = "/usr/local/bin/chromedriver"
+            chrome_driver_binary = "/usr/local/bin/chrome"
             context.browser_args = webdriver.Chrome(chrome_driver_binary, options=chrome_options)
             # context.browser_args = {'options': chrome_options}
 
