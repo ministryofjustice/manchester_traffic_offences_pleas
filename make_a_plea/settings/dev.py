edf5ea07107ace786d086f4995d763413f19cccf
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-11-24 10:20:11 +0000

    add ALLOWED_HOSTS to dev.py

--- make_a_plea/settings/dev.py
+++ make_a_plea/settings/dev.py
@@ -14,3 +14,4 @@ AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+ALLOWED_HOSTS = ['dev-make-a-plea.apps.live.cloud-platform.service.justice.gov.uk']
