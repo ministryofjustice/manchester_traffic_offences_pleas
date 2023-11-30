3bf53ef677378a75dc2ca4da51acf153d5fb77eb
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-11-30 16:18:46 +0000

    fix print statements

--- api/v0/serializers.py
+++ api/v0/serializers.py
@@ -45,13 +45,13 @@ class AuditEventSerializer(serializers.ModelSerializer):
 class CaseSerializer(serializers.ModelSerializer):
     # FIXME: using modelserialiser would suggest not defining fields directly:w
     case_number = serializers.CharField(required=True)
-    print("case_number1: " + case_number)
+    print("case_number1: " + str(case_number))
     urn = serializers.CharField(required=True, validators=[is_valid_urn_format])
-    print("urn1: " + urn)
+    print("urn1: " + str(urn))
     offences = OffenceSerializer(many=True)
-    print("offences: " + offences)
+    print("offences: " + str(offences))
     ou_code = serializers.CharField(required=True)
-    print("ou_code: " + ou_code)
+    print("ou_code: " + str(ou_code))
     accepted_initiation_types = ['J', 'Q', 'S']
 
     def __init__(self, *args, **kwargs):
@@ -121,7 +121,7 @@ class CaseSerializer(serializers.ModelSerializer):
             urn=std_urn,
             case_number=data["case_number"],
             sent=True).exists()
-        print("sent_case: " + sent_case)
+        print("sent_case: " + str(sent_case))
 
         if sent_case:
             AuditEvent().populate(
@@ -143,7 +143,7 @@ class CaseSerializer(serializers.ModelSerializer):
             urn=validated_data["urn"],
             case_number=validated_data["case_number"],
             sent=False)
-        print("open_cases" + open_cases)
+        print("open_cases" + str(open_cases))
 
         if open_cases:
             case = open_cases[0]
@@ -194,9 +194,9 @@ class ResultOffenceSerializer(serializers.ModelSerializer):
 
 class ResultSerializer(serializers.ModelSerializer):
     case_number = serializers.CharField(required=True)
-    print("case_number2: " + case_number)
+    print("case_number2: " + str(case_number))
     urn = serializers.CharField(required=True, validators=[is_valid_urn_format, ])
-    print("urn2: " + urn)
+    print("urn2: " + str(urn))
 
     result_offences = ResultOffenceSerializer(many=True)
 
@@ -217,7 +217,7 @@ class ResultSerializer(serializers.ModelSerializer):
             urn=urn,
             case_number=data["case_number"],
             sent=True).exists()
-        print("sent_results" + sent_results)
+        print("sent_results" + str(sent_results))
 
         if sent_results:
             AuditEvent().populate(
@@ -239,7 +239,7 @@ class ResultSerializer(serializers.ModelSerializer):
             urn=validated_data["urn"],
             case_number=validated_data["case_number"],
             sent=False)
-        print("open_results" + open_results)
+        print("open_results" + str(open_results))
 
         if open_results:
             result = open_results[0]
