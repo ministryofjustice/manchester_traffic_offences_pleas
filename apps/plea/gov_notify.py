d3da2a1ed3cbea5b8124d0da1011a191ce370e60
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-12-06 16:44:03 +0000

    use xhtml2pdf instead of pdfkit

--- apps/plea/gov_notify.py
+++ apps/plea/gov_notify.py
@@ -1,8 +1,8 @@
-import pdfkit
 from django.template.loader import render_to_string
 from notifications_python_client import prepare_upload
 from notifications_python_client.notifications import NotificationsAPIClient
-from typing import Dict
+from xhtml2pdf import pisa
+from io import BytesIO
 
 from make_a_plea.settings.dev import GOV_NOTIFY_API
 
@@ -23,12 +23,17 @@ class GovNotify:
             template_id=self.template_id
         )
 
+    def create_pdf_from_html(self, html: str) -> BytesIO:
+        pdf_stream: BytesIO = BytesIO()
+        pisa.CreatePDF(html, dest=pdf_stream)
+        pdf_stream.seek(0)
+        return pdf_stream
+
     def upload_file_link(self, data, html_template):
         string_data = render_to_string(html_template, data)
         try:
-            pdf_file = pdfkit.from_string(string_data, "plea_file.pdf", verbose=True)
+            pdf_file = create_pdf_from_html(string_data)
             with open(pdf_file, 'rb') as file_:
                 self.personalisation['link_to_file'] = prepare_upload(file_)
         except Exception as e:
             print(f'Error uploading file (plea_file.pdf) to notify with message: {e}')
-
