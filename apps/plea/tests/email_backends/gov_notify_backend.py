3c6a4fa15b49d960bbc09ade389878644759f428
itoulou <touloumbadjiani@gmail.com>
2023-12-13 22:30:28 +0000

    add logs

--- apps/plea/tests/email_backends/gov_notify_backend.py
+++ apps/plea/tests/email_backends/gov_notify_backend.py
@@ -7,6 +7,8 @@ class GovNotifyBackendTest(EmailBackend):
         super().__init__(*args, **kwargs)
 
     def send_messages(self, email_messages):
+        print("IN SEND MESSAGES TEST")
         if email_messages:
             for email in email_messages:
+                print("APPENDING: ", email)
                 notify_mail.outbox.append(email)
