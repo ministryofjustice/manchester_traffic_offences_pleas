d81d7078ad3e630380c5c5200ad5db8275961a42
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-02-22 12:25:51 +0000

    delete raven config from preprod.py

--- api/settings/preprod.py
+++ api/settings/preprod.py
@@ -7,9 +7,4 @@ ADMINS = (
     ('[PREPROD] HMCTS Reform Sustaining Support', 'sustainingteamdev@hmcts.net'),
 )
 
-RAVEN_CONFIG = {
-    'dsn': os.environ["SENTRY_DSN"],
-    'release': os.environ.get("APP_GIT_COMMIT", "no-git-commit-available")
-}
-
 ALLOWED_HOSTS = ['preprod-api-make-a-plea.apps.live.cloud-platform.service.justice.gov.uk']
