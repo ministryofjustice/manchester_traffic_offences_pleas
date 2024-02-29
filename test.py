30ce82b89c29bacc011e1e2c3be8c5c3c32b62ac
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-02-29 12:53:39 +0000

    delete test file

--- test.py
+++
@@ -1,8 +0,0 @@
-import boto3
-import os
-role_arn = os.environ.get("AWS_ROLE_ARN")
-print(role_arn)
-sts_client = boto3.client("sts")
-response = sts_client.assume_role(RoleArn=role_arn, RoleSessionName="AssumeRoleSession")
-print(sts_client)
-print(response)
