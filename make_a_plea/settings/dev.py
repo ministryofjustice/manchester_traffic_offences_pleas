effa2c69dfd53064c8d1aee351ea439090afe929
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-03-05 16:11:48 +0000

    add AWS_S3_REGION_NAME, comment out STATIC_URL, run django in debug mode

--- make_a_plea/settings/dev.py
+++ make_a_plea/settings/dev.py
@@ -1,11 +1,12 @@
 from .docker import *
 
-DJANGO_DEBUG = False
+DJANGO_DEBUG = True
 
 # aws settings
 # AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 # AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = 'eu-west-2'
 AWS_ROLE_ARN = os.getenv('AWS_ROLE_ARN')
 # ECR_ROLE_TO_ASSUME_DEV = os.getenv('ECR_ROLE_TO_ASSUME_DEV')
 AWS_DEFAULT_ACL = 'public-read'
@@ -13,7 +14,7 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 # s3 static settings
 AWS_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 ALLOWED_HOSTS = ['dev-make-a-plea.apps.live.cloud-platform.service.justice.gov.uk', 'dev-api-make-a-plea.apps.live.cloud-platform.service.justice.gov.uk']
 
