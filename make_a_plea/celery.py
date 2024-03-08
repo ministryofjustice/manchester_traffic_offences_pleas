a07ee02a232a1192ec225897e480463c5be2ef2a
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-03-08 13:56:22 +0000

    try a different config in celery.py

--- make_a_plea/celery.py
+++ make_a_plea/celery.py
@@ -2,18 +2,23 @@ from __future__ import absolute_import
 
 import os
 from celery import Celery
+from kombu import Queue
 
 from django.conf import settings
 
 if not "DJANGO_SETTINGS_MODULE" in os.environ:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'make_a_plea.settings.local')
 
-app = Celery('apps.plea', task_create_missing_queues=False)
+app = Celery('apps.plea')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 app.conf.update(
-    broker = 'sqs://',
+    broker_url = 'sqs://',
+    task_queues = [
+        Queue('pet-development-celery')
+    ],
+    task_create_missing_queues = False,
     task_default_queue = 'pet-development-celery',
     broker_transport_options={
         'region': 'eu-west-2',
