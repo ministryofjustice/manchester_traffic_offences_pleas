63deefd28176b8151d91b71a7d8e8975d0bba2da
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-03-11 09:56:59 +0000

    undo last change

--- make_a_plea/celery.py
+++ make_a_plea/celery.py
@@ -3,36 +3,11 @@ from __future__ import absolute_import
 import os
 from celery import Celery
 from kombu import Queue
-import botocore
-import boto3
-import datetime
-from dateutil.tz import tzlocal
 
 from django.conf import settings
 
 if not "DJANGO_SETTINGS_MODULE" in os.environ:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'make_a_plea.settings.local')
-
-assume_role_cache: dict = {}
-def assumed_role_session(role_arn: str, base_session: botocore.session.Session = None):
-    base_session = base_session or boto3.session.Session()._session
-    fetcher = botocore.credentials.AssumeRoleCredentialFetcher(
-        client_creator = base_session.create_client,
-        source_credentials = base_session.get_credentials(),
-        role_arn = role_arn,
-    )
-    creds = botocore.credentials.DeferredRefreshableCredentials(
-        method = 'assume-role',
-        refresh_using = fetcher.fetch_credentials,
-        time_fetcher = lambda: datetime.datetime.now(tzlocal())
-    )
-    botocore_session = botocore.session.Session()
-    botocore_session._credentials = creds
-    return boto3.Session(botocore_session = botocore_session)
-
-session = assumed_role_session('arn:aws:iam::754256621582:policy/cloud-platform/sqs/cloud-platform-sqs-5f3d35c662e8')
-get_sqs_test = session.client('sqs')
-print(get_sqs_test)
 
 app = Celery('apps.plea')
 app.config_from_object('django.conf:settings', namespace='CELERY')
@@ -52,6 +27,6 @@ app.conf.update(
                 'url': 'https://sqs.eu-west-2.amazonaws.com/754256621582/pet-development-celery',
             }
         },
-        session: session,
+        'sts_role_arn': 'arn:aws:iam::754256621582:policy/cloud-platform/sqs/cloud-platform-sqs-5f3d35c662e8',
     }
 )
