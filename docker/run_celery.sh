e3b261f40113c161cca38129e2c164783d8aed07
Mark Butler <mgb777@outlook.com>
2024-03-12 14:59:09 +0000

    Amend readinessprobe command implement sleep in startup shell script

--- docker/run_celery.sh
+++ docker/run_celery.sh
@@ -5,5 +5,11 @@ export PATH=$PATH:/makeaplea/
 export C_FORCE_ROOT=true
 
 #cd /makeaplea && source /makeaplea/docker/celery_defaults && celery worker -A make_a_plea.celery:app --loglevel DEBUG --queues pet-development-celery
-cd /makeaplea && source /makeaplea/docker/celery_defaults
+#cd /makeaplea && source /makeaplea/docker/celery_defaults
+
+while true
+do
+  sleep 60
+done
+
 
