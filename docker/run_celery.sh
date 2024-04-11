bffb4249d06adf87d4ab19e19ce4c6d5bfb81120
Mark Butler <mgb777@outlook.com>
2024-04-11 17:40:43 +0100

    Test to capture email json data in tasks.py
    change run_celery.sh

--- docker/run_celery.sh
+++ docker/run_celery.sh
@@ -4,5 +4,9 @@ export PATH=$PATH:/makeaplea/
 
 export C_FORCE_ROOT=true
 
-cd /makeaplea && source /makeaplea/docker/celery_defaults && celery worker -A make_a_plea.celery:app --loglevel INFO
+#cd /makeaplea && source /makeaplea/docker/celery_defaults && celery worker -A make_a_plea.celery:app --loglevel INFO
 
+while true
+do
+  sleep 60
+done
