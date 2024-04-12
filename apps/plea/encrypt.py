a31118da43ec630a5e9621b2ee29785b94a85493
Mark Butler <mgb777@outlook.com>
2024-04-12 15:43:17 +0100

    Remove no-home-dir from Dockerfile (needed for gpg) Add encryption debugging

--- apps/plea/encrypt.py
+++ apps/plea/encrypt.py
@@ -2,6 +2,7 @@ import gnupg
 import json
 import os
 import time
+import logging
 
 from django.conf import settings
 from django.core.serializers.json import DjangoJSONEncoder
@@ -14,6 +15,7 @@ gpg.encoding = 'utf-8'
 class PersistenceError(Exception):
     pass
 
+logger = logging.getLogger(__name__)
 
 def clear_user_data():
     """
@@ -44,28 +46,45 @@ def encrypt_and_store_user_data(urn, case_id, data, user_data_directory=None):
 
     """
 
+    logger.warning("encrypt_and_store_user_data urn: {} case: {}".format(urn,case_id))
+
     file_name = "{}_[{}]_{}.data.gpg".format(urn.replace('/', '-').upper(), case_id,
                                         str(time.time()).replace('.', '_'))
+    logger.warning("encrypt_and_store_user_data: file_name: {}".format(file_name))
 
     file_path = os.path.join(settings.USER_DATA_DIRECTORY, file_name)
+    logger.warning("encrypt_and_store_user_data: file_path: {}".format(file_path))
 
     data = json.dumps(data, cls=DjangoJSONEncoder)
+    h = open("before_gpg.encrypt.txt", "w")
+    h.write(data)
+    h.flush()
+    h.close()
 
+    logger.warning("encrypt_and_store_user_data: about to encrypt data")
     encrypted_data = gpg.encrypt(data, settings.GPG_RECIPIENT, always_trust=True)
+    logger.warning("encrypt_and_store_user_data: encrypted_data.status {}".format(encrypted_data.status))
 
     if encrypted_data.status != "encryption ok":
+        logger.error("encrypt_and_store_user_data: encrypted_data.status {}".format(encrypted_data.status))
         raise PersistenceError(
             "GPG encryption failed: {}".format(encrypted_data.status))
 
     try:
+        logger.warning("encrypt_and_store_user_data: creating file {}".format(file_path))
         fd = os.open(file_path, os.O_CREAT | os.O_EXCL | os.O_WRONLY)
+        logger.warning("encrypt_and_store_user_data: file created")
         os.write(fd, str(encrypted_data).encode())
+        logger.warning("encrypt_and_store_user_data: file write")
 
         # NOTE: It seems unlikely that there'll be file name
         # clashes given the file name is the urn + timestamp. However,
         # if it does become an issue, you can check for a 'file already
         # exists' error by captching an OSError with ex.errno == 17
+    except Exception as e:
+        logger.error("encrypt_and_store_user_data fd file error: {}".format(e))
 
     finally:
         if 'fd' in locals():
+            logger.warning("encrypt_and_store_user_data: closing file")
             os.close(fd)
