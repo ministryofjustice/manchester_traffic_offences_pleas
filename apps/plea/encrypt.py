e847668fd481c3d54fb094fb77a875f61be508de
Mark Butler <mgb777@outlook.com>
2024-04-18 15:44:27 +0100

    missing space in file path logging

--- apps/plea/encrypt.py
+++ apps/plea/encrypt.py
@@ -62,7 +62,7 @@ def encrypt_and_store_user_data(urn, case_id, data, user_data_directory=None):
     try:
         fd = os.open(file_path, os.O_CREAT | os.O_EXCL | os.O_WRONLY)
         os.write(fd, str(encrypted_data).encode())
-        logger.info("encrypt_and_store_user_data: file path{}".format(file_path))
+        logger.info("encrypt_and_store_user_data: file path {}".format(file_path))
 
         # NOTE: It seems unlikely that there'll be file name
         # clashes given the file name is the urn + timestamp. However,
