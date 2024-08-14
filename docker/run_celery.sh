164dea01e145f01236a2ead20a62220c2db8d213
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-08-14 16:28:32 +0100

    update the celery worker command in run_celery.sh to work with new version of celery

--- docker/run_celery.sh
+++ docker/run_celery.sh
@@ -4,9 +4,4 @@ export PATH=$PATH:/makeaplea/
 
 export C_FORCE_ROOT=true
 
-# cd /makeaplea && source /makeaplea/docker/celery_defaults && celery worker -A make_a_plea.celery:app --loglevel DEBUG --queues pet-development-celery
-
-while true
-do
-  sleep 60
-done
+cd /makeaplea && source /makeaplea/docker/celery_defaults && celery --app=make_a_plea.celery:app worker --loglevel DEBUG --queues pet-development-celery
