b4eda684820d5f91dec3bb0dd25d530769d1adb9
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-08-19 10:44:19 +0100

    attempt to run celery in the background

--- docker/run_celery.sh
+++ docker/run_celery.sh
@@ -4,4 +4,4 @@ export PATH=$PATH:/makeaplea/
 
 export C_FORCE_ROOT=true
 
-cd /makeaplea && source /makeaplea/docker/celery_defaults && celery --app=make_a_plea.celery:app worker --loglevel DEBUG --queues pet-development-celery &
+cd /makeaplea && source /makeaplea/docker/celery_defaults && nohup celery --app=make_a_plea.celery:app worker --loglevel DEBUG --queues pet-development-celery &
