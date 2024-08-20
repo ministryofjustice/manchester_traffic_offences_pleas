419b8f178435e6df694565658366e34f1ea4d7b7
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-08-20 16:06:49 +0100

    have a go at running the celery command as well as an infinite loop

--- docker/run_celery.sh
+++ docker/run_celery.sh
@@ -4,10 +4,12 @@ export PATH=$PATH:/makeaplea/
 export C_FORCE_ROOT=true
 
 # Source the Celery defaults
-source /makeaplea/docker/celery_defaults
+# source /makeaplea/docker/celery_defaults
 
 # Start the Celery worker
 # celery --app=make_a_plea.celery:app worker --loglevel DEBUG --queues pet-development-celery
+
+cd /makeaplea && source /makeaplea/docker/celery_defaults && celery --app=make_a_plea.celery:app worker --loglevel DEBUG --queues pet-development-celery &
 
 while true
 do
