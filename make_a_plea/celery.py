a9c37e7f10ffb4962e5a332fd7b0c34a9fbc42fb
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-08-20 16:59:33 +0100

    remove AWS_SQS_QUEUE_URL from celery.py

--- make_a_plea/celery.py
+++ make_a_plea/celery.py
@@ -9,6 +9,5 @@ if not "DJANGO_SETTINGS_MODULE" in os.environ:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'make_a_plea.settings.local')
 
 app = Celery('apps.plea')
-app.conf.broker_url = settings.AWS_SQS_QUEUE_URL
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
