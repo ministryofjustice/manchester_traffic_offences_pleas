8728960194b32c6f77b59cf1f90c0e8b167e4f4d
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-08-22 16:58:25 +0100

    run a while loop in celery container again to experiment some more

--- docker/run_celery.sh
+++ docker/run_celery.sh
@@ -3,4 +3,9 @@
 export PATH=$PATH:/makeaplea/
 export C_FORCE_ROOT=true
 
-cd /makeaplea && source /makeaplea/docker/celery_defaults && celery -A make_a_plea.celeryapp worker --loglevel=info --queues pet-development-celery
+# cd /makeaplea && source /makeaplea/docker/celery_defaults && celery -A make_a_plea.celeryapp worker --loglevel=info --queues pet-development-celery
+
+while true
+do
+  sleep 60
+done
