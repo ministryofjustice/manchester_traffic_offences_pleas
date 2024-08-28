f2f4db62a509f597d598d4555822aa645b8ea60e
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-08-28 10:13:16 +0100

    update celery settings in preprod.py

--- make_a_plea/settings/preprod.py
+++ make_a_plea/settings/preprod.py
@@ -2,21 +2,19 @@ from .docker import *
 import os
 
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME_PREPROD')
-ECR_ROLE_TO_ASSUME_PREPROD = os.getenv('ECR_ROLE_TO_ASSUME_PREPROD')
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-# s3 static settings
 AWS_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-ALLOWED_HOSTS = ['preprod-make-a-plea.apps.live.cloud-platform.service.justice.gov.uk']
-
-CELERY_TASK_ALWAYS_EAGER = os.environ.get("CELERY_ALWAYS_EAGER", False)
+ALLOWED_HOSTS = ['preprod-make-a-plea.apps.live.cloud-platform.service.justice.gov.uk', 'preprod-api-make-a-plea.apps.live.cloud-platform.service.justice.gov.uk']
 CELERY_BROKER_URL = "SQS://"
+CELERY_TASK_DEFAULT_QUEUE = "pet-preproduction-celery"
 CELERY_BROKER_TRANSPORT_OPTIONS = {
     'region': 'eu-west-2',
-    'queue_name_prefix': "pet-preproduction-",
-    'polling_interval': 1,
-    'visibility_timeout': 3600
+    'predefined_queues': {
+        'pet-preproduction-celery': {  ## the name of the SQS queue
+            'url': 'https://sqs.eu-west-2.amazonaws.com/754256621582/pet-preproduction-celery',
+        }
+    }
 }
