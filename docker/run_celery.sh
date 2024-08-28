511b4780529fe0b5450e8ca69921044877b22d6f
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-08-28 11:33:24 +0100

    attempt to update the config so that run_celery.sh picks up the env name and runs on the correct sqs

--- docker/run_celery.sh
+++ docker/run_celery.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+ENV_NAME=$1
+
 export PATH=$PATH:/makeaplea/
 export C_FORCE_ROOT=true
 
 # Run the migrations here?
 # python manage.py migrate --noinput
 
-cd /makeaplea && source /makeaplea/docker/celery_defaults && celery -A make_a_plea.celery worker --loglevel=info --queues pet-development-celery
+cd /makeaplea && source /makeaplea/docker/celery_defaults && celery -A make_a_plea.celery worker --loglevel=info --queues pet-$ENV_NAME-celery
