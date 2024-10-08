4db6e5aa7fe78610a3e3a00d636e741f4e92c900
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-10-08 09:47:52 +0100

    add some logging to celery.py, undo previous change

--- make_a_plea/celery.py
+++ make_a_plea/celery.py
@@ -1,13 +1,17 @@
 from __future__ import absolute_import
 
 import os
+print("Before Celery import")
 from celery import Celery
+print("After Celery import")
 
 from django.conf import settings
 
-if not "DJANGO_SETTINGS_MODULE" in os.environ:
+if "DJANGO_SETTINGS_MODULE" not in os.environ:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'make_a_plea.settings.local')
 
 app = Celery('celeryapp')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+print("Celery app created")
