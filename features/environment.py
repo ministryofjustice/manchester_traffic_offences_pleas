b60fe66cf8c169a9e52527a93a3f53924623212b
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-10-09 15:15:52 +0100

    try different chrome driver options

--- features/environment.py
+++ features/environment.py
@@ -70,10 +70,10 @@ def before_all(context):
             chrome_options.add_argument("--headless")
             chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument("--no-sandbox")
-            chrome_options.add_argument("--window-size=1920,1080")
+            chrome_options.add_argument("--disable-extensions")
             context.browser_args = {'options': chrome_options}
 
-    context.browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    context.browser = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=chrome_options)
 
     context.base_url = config['base_url']
 
