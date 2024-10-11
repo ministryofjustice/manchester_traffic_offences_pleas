c6c0913ebc4b081cb20ed3c0d4d9c405075ef6b5
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-10-11 15:04:55 +0100

    update some more django.urls imports

--- make_a_plea/urls.py
+++ make_a_plea/urls.py
@@ -4,7 +4,7 @@ from moj_irat.views import PingJsonView, HealthcheckView
 
 from . import views
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import re_path as url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView, RedirectView
