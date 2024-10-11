6695ae942967a04a4850f857abe7128ea12a6ffa
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-10-11 10:02:21 +0100

    change assignment_tag to simple_tag

--- apps/plea/templatetags/admin_report_templatetags.py
+++ apps/plea/templatetags/admin_report_templatetags.py
@@ -7,7 +7,7 @@ register = template.Library()
 def month_name(month_num):
     return calendar.month_name[month_num]
 
-@register.assignment_tag(takes_context=True)
+@register.simple_tag(takes_context=True)
 def percentage(context, num, total_num):
     """
     Works out the percentage of num over total_num and then appends the percentage sign
