16cd145798eeaa7ecfc8c044b083b56f400644f6
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-10-14 11:07:54 +0100

    undo last change, specify localhost and port in base.py

--- make_a_plea/settings/base.py
+++ make_a_plea/settings/base.py
@@ -27,8 +27,8 @@ DATABASES = {
         'NAME': 'manchester_traffic_offences',
         'USER': 'postgres',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
