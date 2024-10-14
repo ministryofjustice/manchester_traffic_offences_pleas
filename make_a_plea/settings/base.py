2f2f552c90ffa9eec4153c086f46f53472ad7e30
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-10-14 11:22:04 +0100

    update the database engine name to django.db.backends.postgresql

--- make_a_plea/settings/base.py
+++ make_a_plea/settings/base.py
@@ -23,12 +23,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'manchester_traffic_offences',
         'USER': 'postgres',
         'PASSWORD': '',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
