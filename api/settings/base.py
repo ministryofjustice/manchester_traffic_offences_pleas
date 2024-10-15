8ba49016a4080e2e805f44627fb3486d6918b572
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-10-15 14:44:06 +0100

    add django_components.safer_staticfiles to INSTALLED_APPS

--- api/settings/base.py
+++ api/settings/base.py
@@ -12,6 +12,7 @@ INSTALLED_APPS = [
     # 'django.contrib.staticfiles',
     'django_extensions',
     'django_components',
+    'django_components.safer_staticfiles',
     'rest_framework',
     'reports'
 ]
