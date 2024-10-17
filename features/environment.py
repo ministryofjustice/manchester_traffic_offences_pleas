3119819fbeba3f5e21f2af6fc0abd3a9c8a8f790
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-10-17 14:44:45 +0100

    remove "--remote-debugging-port=9222" from chrome_options

--- features/environment.py
+++ features/environment.py
@@ -69,7 +69,7 @@ def before_all(context):
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--remote-debugging-port=9222")
+        # chrome_options.add_argument("--remote-debugging-port=9222")
         context.browser_args = {'options': chrome_options}
 
     context.base_url = config['base_url']
