8776cc515402633715051da548267228339543fd
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-10-18 17:09:01 +0100

    delete duplicate step

--- features/steps/welsh_validation.py
+++ features/steps/welsh_validation.py
@@ -12,5 +12,4 @@ def step_impl(context):
     except TimeoutException:
         print(f"Welsh validation message not found")
         print(f"Current URL: {context.browser.current_url}")
-        print(f"Page source:\n{context.browser.page_source}")
         raise AssertionError(f"Welsh validation message not found")
