9cd19ba4567a141ae08019912a0bd9908a852b6c
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-10-18 17:14:39 +0100

    remove duplicate step

--- features/steps/welsh_validation.py
+++ features/steps/welsh_validation.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
 @then(u'I should see the Welsh validation message')
